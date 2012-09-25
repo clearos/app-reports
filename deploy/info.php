838de68af7d8a734a48928abef9236950fde15e6
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-09-25 21:10:29 +0000

    - Fixed last check-in: forgot to delete files from SVN

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'reports';
-$app['version'] = '1.2.1';
+$app['version'] = '1.2.2';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
