8534953e045a2f57b345d9ef8fde22f9bf83fb41
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-10-26 16:35:19 +0000

    - Moved translations to base

--- language/en_US/reports_lang.php
+++ language/en_US/reports_lang.php
@@ -2,11 +2,6 @@
 
 $lang['reports_app_name'] = 'Base Reports';
 $lang['reports_app_description'] = 'The Base Reports app provides a set of standard report tools for the operating system.';
-$lang['reports_free'] = 'Free';
-$lang['reports_cached'] = 'Cached';
-$lang['reports_buffers'] = 'Buffers';
-$lang['reports_kernel_and_applications'] = 'Kernel and Applications';
-$lang['reports_memory_information'] = 'Memory Information';
 $lang['reports_today'] = 'Today';
 $lang['reports_yesterday'] = 'Yesterday';
 $lang['reports_last_30_days'] = 'Last 30 Days';
