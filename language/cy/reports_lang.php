d9ad7fa7e6fdfa8f82c76b31070de985509204d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:52:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Mae&#39;r app Adroddiadau Base yn darparu set o offer adroddiad safonol ar gyfer y system weithredu.';
+$lang['reports_app_name'] = 'Adroddiadau sylfaen';
+$lang['reports_date_range'] = 'Ystod Dyddiadau';
+$lang['reports_device_type_summary'] = 'Dyfais Math Crynodeb';
+$lang['reports_device_types'] = 'Mathau dyfais';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Adroddiad llawn';
+$lang['reports_ip_summary'] = 'Crynodeb IP';
+$lang['reports_last_30_days'] = '30 Dyddiau Olaf';
+$lang['reports_last_7_days'] = '7 Diwrnod Diwethaf';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Ystod yn annilys.';
+$lang['reports_report_data'] = 'Data adroddiad';
+$lang['reports_report_settings'] = 'Gosodiadau adroddiad';
+$lang['reports_reports'] = 'adroddiadau';
+$lang['reports_today'] = 'heddiw';
+$lang['reports_top_device_types'] = 'Mathau Dyfais Top';
+$lang['reports_top_external_ips'] = 'Top IPs Allanol';
+$lang['reports_top_ips'] = 'IPs Top';
+$lang['reports_top_users'] = 'Defnyddwyr Top';
+$lang['reports_user_summary'] = 'Crynodeb defnyddiwr';
+$lang['reports_yesterday'] = 'ddoe';
