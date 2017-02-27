3a1ddc0fa00bedc505dbd5ea0f9c4544b18e1f1e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:51:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Додаток базових звітів надає набір стандартних інструментів побудови звітів для операційної системи.';
+$lang['reports_app_name'] = 'базові звіти';
+$lang['reports_date_range'] = 'Проміжок часу';
+$lang['reports_device_type_summary'] = 'Тип пристрою Додаткова інформація';
+$lang['reports_device_types'] = 'типи пристроїв';
+$lang['reports_entries'] = 'записи';
+$lang['reports_full_report'] = 'повний звіт';
+$lang['reports_ip_summary'] = 'IP Додаткова інформація';
+$lang['reports_last_30_days'] = 'Останні 30 днів';
+$lang['reports_last_7_days'] = 'За останні 7 днів';
+$lang['reports_live'] = 'жива';
+$lang['reports_range_invalid'] = 'Діапазон є недійсним.';
+$lang['reports_report_data'] = 'дані звіту';
+$lang['reports_report_settings'] = 'налаштування звіту';
+$lang['reports_reports'] = 'Звіти';
+$lang['reports_today'] = 'сьогодні';
+$lang['reports_top_device_types'] = 'Кращі типи пристроїв';
+$lang['reports_top_external_ips'] = 'Топ Зовнішні IP-адреси';
+$lang['reports_top_ips'] = 'Кращі IP-адреси';
+$lang['reports_top_users'] = 'Топ користувачів';
+$lang['reports_user_summary'] = 'резюме користувача';
+$lang['reports_yesterday'] = 'вчора';
