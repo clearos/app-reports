02af88ec4d11faf955ee6e5231f84be81944fa03
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:51:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Baz Raporları uygulaması işletim sistemi için standart rapor araçları kümesi sağlar.';
+$lang['reports_app_name'] = 'baz Raporları';
+$lang['reports_date_range'] = 'Tarih aralığı';
+$lang['reports_device_type_summary'] = 'Cihaz Türü Özeti';
+$lang['reports_device_types'] = 'cihaz Çeşitleri';
+$lang['reports_entries'] = 'Girdileri';
+$lang['reports_full_report'] = 'tam Raporu';
+$lang['reports_ip_summary'] = 'IP Özeti';
+$lang['reports_last_30_days'] = 'Son 30 gün';
+$lang['reports_last_7_days'] = 'Son 7 gün';
+$lang['reports_live'] = 'Canlı';
+$lang['reports_range_invalid'] = 'Aralığı geçersiz.';
+$lang['reports_report_data'] = 'Rapor Verileri';
+$lang['reports_report_settings'] = 'Rapor Ayarları';
+$lang['reports_reports'] = 'Raporlar';
+$lang['reports_today'] = 'Bugün';
+$lang['reports_top_device_types'] = 'En Cihaz Türleri';
+$lang['reports_top_external_ips'] = 'En Dış IP&#39;leri';
+$lang['reports_top_ips'] = 'En IP&#39;ler';
+$lang['reports_top_users'] = 'En Kullanıcılar';
+$lang['reports_user_summary'] = 'kullanıcı Özeti';
+$lang['reports_yesterday'] = 'Dün';
