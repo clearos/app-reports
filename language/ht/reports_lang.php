af92584855796778bf937655d8523f0142678472
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:42:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'App a Rapò Sèvi bay yon seri zouti rapò estanda pou sistèm nan fonksyone.';
+$lang['reports_app_name'] = 'Rapò Sèvi';
+$lang['reports_date_range'] = 'Dat Range';
+$lang['reports_device_type_summary'] = 'Aparèy Kalite Rezime';
+$lang['reports_device_types'] = 'Kalite Aparèy';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'tout Rapò';
+$lang['reports_ip_summary'] = 'IP Rezime';
+$lang['reports_last_30_days'] = 'Pase 30 jou';
+$lang['reports_last_7_days'] = 'Dènye 7 jou';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range se valab.';
+$lang['reports_report_data'] = 'Rapò Done';
+$lang['reports_report_settings'] = 'Rapò Anviwònman';
+$lang['reports_reports'] = 'Rapò';
+$lang['reports_today'] = 'jodi a';
+$lang['reports_top_device_types'] = 'Top Kalite Aparèy';
+$lang['reports_top_external_ips'] = 'Top Eksteryè ONSÈY';
+$lang['reports_top_ips'] = 'Top ONSÈY';
+$lang['reports_top_users'] = 'Top Itilizatè yo';
+$lang['reports_user_summary'] = 'Rezime itilizatè';
+$lang['reports_yesterday'] = 'yè';
