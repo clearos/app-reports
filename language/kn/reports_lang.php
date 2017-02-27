80e59988d0b9d18d946cffb350e721f8dde8cbd9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:45:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'ಬೇಸ್ ವರದಿಗಳು ಅಪ್ಲಿಕೇಶನ್ ಆಪರೇಟಿಂಗ್ ಸಿಸ್ಟಮ್ ಗುಣಮಟ್ಟ ವರದಿ ಸಾಧನಗಳನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['reports_app_name'] = 'ಬೇಸ್ ವರದಿಗಳು';
+$lang['reports_date_range'] = 'ದಿನಾಂಕ ಶ್ರೇಣಿ';
+$lang['reports_device_type_summary'] = 'ಸಾಧನದ ಪ್ರಕಾರ ಸಾರಾಂಶ';
+$lang['reports_device_types'] = 'ಸಾಧನದ ಪ್ರಕಾರಗಳನ್ನು';
+$lang['reports_entries'] = 'ನಮೂದುಗಳು';
+$lang['reports_full_report'] = 'ಫುಲ್ ರಿಪೋರ್ಟ್';
+$lang['reports_ip_summary'] = 'ಐಪಿ ಸಾರಾಂಶ';
+$lang['reports_last_30_days'] = 'ಕೊನೆಯ 30 ದಿನಗಳು';
+$lang['reports_last_7_days'] = 'ಕೊನೆಯ 7 ದಿನಗಳು';
+$lang['reports_live'] = 'ಲೈವ್';
+$lang['reports_range_invalid'] = 'ರೇಂಜ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['reports_report_data'] = 'ವರದಿ ದತ್ತಾಂಶವನ್ನು';
+$lang['reports_report_settings'] = 'ವರದಿ ಸೆಟ್ಟಿಂಗ್ಗಳು';
+$lang['reports_reports'] = 'ವರದಿಗಳು';
+$lang['reports_today'] = 'ಇಂದು';
+$lang['reports_top_device_types'] = 'ಟಾಪ್ ಸಾಧನ ವಿಧಗಳು';
+$lang['reports_top_external_ips'] = 'ಟಾಪ್ ಬಾಹ್ಯ ಐಪಿಎಸ್';
+$lang['reports_top_ips'] = 'ಟಾಪ್ ಐಪಿಎಸ್';
+$lang['reports_top_users'] = 'ಅಗ್ರ ಬಳಕೆದಾರರು';
+$lang['reports_user_summary'] = 'ಬಳಕೆದಾರ ಸಾರಾಂಶ';
+$lang['reports_yesterday'] = 'ನಿನ್ನೆ';
