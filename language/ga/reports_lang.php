ec91eb0035238cb2324b1532fe9f2926b011a751
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:43:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Soláthraíonn an app Tuarascálacha Base sraith uirlisí tuarascála caighdeánach do chóras oibriúcháin.';
+$lang['reports_app_name'] = 'Base Tuarascálacha';
+$lang['reports_date_range'] = 'dáta Raon';
+$lang['reports_device_type_summary'] = 'Gléas Cineál Achoimre';
+$lang['reports_device_types'] = 'Cineálacha gléas';
+$lang['reports_entries'] = 'Iontrálacha';
+$lang['reports_full_report'] = 'Tuarascáil Iomlán';
+$lang['reports_ip_summary'] = 'Achoimre IP';
+$lang['reports_last_30_days'] = 'Last 30 Days';
+$lang['reports_last_7_days'] = 'Last 7 Days';
+$lang['reports_live'] = 'beo';
+$lang['reports_range_invalid'] = 'Is raon neamhbhailí.';
+$lang['reports_report_data'] = 'Tuairisc Sonraí';
+$lang['reports_report_settings'] = 'Socruithe Tuarascáil';
+$lang['reports_reports'] = 'tuarascálacha';
+$lang['reports_today'] = 'Inniu';
+$lang['reports_top_device_types'] = 'Cineálacha Gléas Barr';
+$lang['reports_top_external_ips'] = 'Barr IPs Seachtrach';
+$lang['reports_top_ips'] = 'IPs barr';
+$lang['reports_top_users'] = 'úsáideoirí';
+$lang['reports_user_summary'] = 'Úsáideoir Achoimre';
+$lang['reports_yesterday'] = 'Inné';
