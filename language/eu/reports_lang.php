609e38f9dfa5c5ee1a00e42d105c0dbf4664c92d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:39:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Base Txostenak aplikazioa txosten Tresna estandar multzo bat sistema eragilearen eskaintzen.';
+$lang['reports_app_name'] = 'Base Txostenak';
+$lang['reports_date_range'] = 'data tartea';
+$lang['reports_device_type_summary'] = 'Gailu mota Laburpena';
+$lang['reports_device_types'] = 'gailu motak';
+$lang['reports_entries'] = 'sarrerak';
+$lang['reports_full_report'] = 'Txosten osoa';
+$lang['reports_ip_summary'] = 'IP Laburpena';
+$lang['reports_last_30_days'] = 'Azken 30 egunak';
+$lang['reports_last_7_days'] = 'Azken 7 egunak';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range baliogabea da.';
+$lang['reports_report_data'] = 'txostena datuak';
+$lang['reports_report_settings'] = 'txostena ezarpenak';
+$lang['reports_reports'] = 'Txostenak';
+$lang['reports_today'] = 'gaur';
+$lang['reports_top_device_types'] = 'Top gailu motak';
+$lang['reports_top_external_ips'] = 'Top Kanpo IPak';
+$lang['reports_top_ips'] = 'Top IPak';
+$lang['reports_top_users'] = 'Top erabiltzaileak';
+$lang['reports_user_summary'] = 'Erabiltzaile Laburpena';
+$lang['reports_yesterday'] = 'Atzo';
