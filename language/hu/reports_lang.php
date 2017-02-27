ff5149cd809bcd49daf8f4e7700a2fe967e80f53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:43:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'A Base Reports app nyújt egy sor szabványos jelentés eszközök az operációs rendszer.';
+$lang['reports_app_name'] = 'Base jelentések';
+$lang['reports_date_range'] = 'Időintervallum';
+$lang['reports_device_type_summary'] = 'A készülék típusa Összefoglaló';
+$lang['reports_device_types'] = 'készülék típusok';
+$lang['reports_entries'] = 'Bejegyzés';
+$lang['reports_full_report'] = 'teljes jelentés';
+$lang['reports_ip_summary'] = 'IP-összefoglaló';
+$lang['reports_last_30_days'] = 'Elmúlt 30 nap';
+$lang['reports_last_7_days'] = 'Utolsó 7 nap';
+$lang['reports_live'] = 'Élő';
+$lang['reports_range_invalid'] = 'Tartomány érvénytelen.';
+$lang['reports_report_data'] = 'jelentés adatai';
+$lang['reports_report_settings'] = 'jelentés beállítások';
+$lang['reports_reports'] = 'jelentések';
+$lang['reports_today'] = 'Ma';
+$lang['reports_top_device_types'] = 'Top Device Types';
+$lang['reports_top_external_ips'] = 'Top külső IP';
+$lang['reports_top_ips'] = 'Top IP';
+$lang['reports_top_users'] = 'Top felhasználók';
+$lang['reports_user_summary'] = 'Felhasználó Összefoglaló';
+$lang['reports_yesterday'] = 'Tegnap';
