0fb3630f0f24ce2f98efdfbf65095131c111fd41
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:45:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Ny Base Reports fampiharana manome tatitra fitsipika napetraka ny fitaovana ho an&#39;ny rafitra fandidiana.';
+$lang['reports_app_name'] = 'Base Reports';
+$lang['reports_date_range'] = 'Date Range';
+$lang['reports_device_type_summary'] = 'Device Type Summary';
+$lang['reports_device_types'] = 'Device Types';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Full Report';
+$lang['reports_ip_summary'] = 'IP Summary';
+$lang['reports_last_30_days'] = 'Last 30 Days';
+$lang['reports_last_7_days'] = 'Last 7 Days';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range dia tsy manan-kery.';
+$lang['reports_report_data'] = 'Report Data';
+$lang['reports_report_settings'] = 'Report Settings';
+$lang['reports_reports'] = 'Reports';
+$lang['reports_today'] = 'ankehitriny';
+$lang['reports_top_device_types'] = 'Top Device Types';
+$lang['reports_top_external_ips'] = 'Top External IPS';
+$lang['reports_top_ips'] = 'Top IPS';
+$lang['reports_top_users'] = 'Top Users';
+$lang['reports_user_summary'] = 'User Summary';
+$lang['reports_yesterday'] = 'omaly';
