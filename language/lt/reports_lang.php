97472e57d42a9a41d62e9d261b353554f29828e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:45:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Bazinė Ataskaitos programa suteikia standartinę ataskaitą įrankių rinkinį operacinę sistemą.';
+$lang['reports_app_name'] = 'Netaurieji ataskaitos';
+$lang['reports_date_range'] = 'Data asortimentas';
+$lang['reports_device_type_summary'] = 'Įrenginio tipas santrauka';
+$lang['reports_device_types'] = 'prietaiso tipai';
+$lang['reports_entries'] = 'įrašai';
+$lang['reports_full_report'] = 'Visas pranešimas';
+$lang['reports_ip_summary'] = 'IP santrauka';
+$lang['reports_last_30_days'] = 'Paskutines 30 dienų';
+$lang['reports_last_7_days'] = 'Vėliausiai per 7 dienas';
+$lang['reports_live'] = 'gyvai';
+$lang['reports_range_invalid'] = 'Kategorijos negalioja.';
+$lang['reports_report_data'] = 'ataskaita duomenų';
+$lang['reports_report_settings'] = 'Pranešimas Nustatymai';
+$lang['reports_reports'] = 'ataskaitos';
+$lang['reports_today'] = 'šiandien';
+$lang['reports_top_device_types'] = 'Populiariausi įrenginių tipai';
+$lang['reports_top_external_ips'] = 'Į viršų Išorinis IP';
+$lang['reports_top_ips'] = 'Geriausi IP';
+$lang['reports_top_users'] = 'Aktyviausi vartotojai';
+$lang['reports_user_summary'] = 'vartotojas santrauka';
+$lang['reports_yesterday'] = 'vakar';
