5ada1ec7818ce04aa3f7593719539fefb3f211b9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:39:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'ويقدم التطبيق تقارير قاعدة مجموعة من الأدوات تقرير القياسية لنظام التشغيل.';
+$lang['reports_app_name'] = 'تقارير قاعدة';
+$lang['reports_date_range'] = 'نطاق الموعد';
+$lang['reports_device_type_summary'] = 'نوع الجهاز موجز';
+$lang['reports_device_types'] = 'أنواع الأجهزة';
+$lang['reports_entries'] = 'مقالات';
+$lang['reports_full_report'] = 'تقرير كامل';
+$lang['reports_ip_summary'] = 'ملخص IP';
+$lang['reports_last_30_days'] = 'آخر 30 أيام';
+$lang['reports_last_7_days'] = 'آخر 7 أيام';
+$lang['reports_live'] = 'حي';
+$lang['reports_range_invalid'] = 'مجموعة غير صالح.';
+$lang['reports_report_data'] = 'بيانات التقرير';
+$lang['reports_report_settings'] = 'إعدادات تقرير';
+$lang['reports_reports'] = 'تقارير';
+$lang['reports_today'] = 'اليوم';
+$lang['reports_top_device_types'] = 'أنواع الأجهزة العليا';
+$lang['reports_top_external_ips'] = 'أعلى المتكاملة الخارجية';
+$lang['reports_top_ips'] = 'أعلى المتكاملة';
+$lang['reports_top_users'] = 'أعلى الصفحة عدد';
+$lang['reports_user_summary'] = 'ملخص المستخدم';
+$lang['reports_yesterday'] = 'في الامس';
