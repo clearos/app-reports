b12be84e9ca46860284dead595b01caf837bc1f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:45:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Bāzes Ziņojumi app paredz virkni standarta atskaišu rīkus operētājsistēmu.';
+$lang['reports_app_name'] = 'Base Ziņojumi';
+$lang['reports_date_range'] = 'datumu diapazons';
+$lang['reports_device_type_summary'] = 'Device Type kopsavilkums';
+$lang['reports_device_types'] = 'ierīču veidi';
+$lang['reports_entries'] = 'ieraksti';
+$lang['reports_full_report'] = 'pilna pārskats';
+$lang['reports_ip_summary'] = 'IP kopsavilkums';
+$lang['reports_last_30_days'] = 'Pēdējās 30 dienas';
+$lang['reports_last_7_days'] = 'Pēdējās 7 dienas';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Diapazons ir nederīgs.';
+$lang['reports_report_data'] = 'ziņojums Data';
+$lang['reports_report_settings'] = 'ziņojums iestatījumi';
+$lang['reports_reports'] = 'ziņojumi';
+$lang['reports_today'] = 'šodien';
+$lang['reports_top_device_types'] = 'Top Ierīču veidi';
+$lang['reports_top_external_ips'] = 'Top Ārējā IP';
+$lang['reports_top_ips'] = 'Top IP';
+$lang['reports_top_users'] = 'Top lietotāji';
+$lang['reports_user_summary'] = 'lietotājs kopsavilkums';
+$lang['reports_yesterday'] = 'vakar';
