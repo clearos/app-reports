b1d6d1b9fff3074ef3f6eebab9022f37008835a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:45:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Renuntiationes app copia praebet basim instrumenta famae vexillum operating ratio.';
+$lang['reports_app_name'] = 'Base Reports';
+$lang['reports_date_range'] = 'Range Date';
+$lang['reports_device_type_summary'] = 'Fabrica Libri Type';
+$lang['reports_device_types'] = 'Genera fabrica';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Full Report';
+$lang['reports_ip_summary'] = 'Libri IV';
+$lang['reports_last_30_days'] = 'Ultimum XXX dies';
+$lang['reports_last_7_days'] = 'VII diebus novissimis';
+$lang['reports_live'] = 'Ago';
+$lang['reports_range_invalid'] = 'Range est invalidum.';
+$lang['reports_report_data'] = 'Data Report';
+$lang['reports_report_settings'] = 'Occasus Report';
+$lang['reports_reports'] = 'Reports';
+$lang['reports_today'] = 'Hodie,';
+$lang['reports_top_device_types'] = 'Top Genera Fabrica';
+$lang['reports_top_external_ips'] = 'Top External IPS';
+$lang['reports_top_ips'] = 'Top IPS';
+$lang['reports_top_users'] = 'Top';
+$lang['reports_user_summary'] = 'User Summary';
+$lang['reports_yesterday'] = 'Heri';
