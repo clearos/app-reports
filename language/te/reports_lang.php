de438f0014cbea2fa1898b7286dd6f5b3599e81c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:50:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'బేస్ నివేదికలు అనువర్తనం ఆపరేటింగ్ సిస్టమ్ కోసం ప్రామాణిక నివేదిక సాధనాల సమితిని అందిస్తుంది.';
+$lang['reports_app_name'] = 'బేస్ నివేదికలు';
+$lang['reports_date_range'] = 'తేదీ పరిధి';
+$lang['reports_device_type_summary'] = 'పరికర రకం సారాంశం';
+$lang['reports_device_types'] = 'పరికర రకాలు';
+$lang['reports_entries'] = 'ఎంట్రీలు';
+$lang['reports_full_report'] = 'పూర్తి నివేదిక';
+$lang['reports_ip_summary'] = 'ఐపీ సారాంశం';
+$lang['reports_last_30_days'] = 'గత 30 డేస్';
+$lang['reports_last_7_days'] = 'చివరి 7 రోజులు';
+$lang['reports_live'] = 'ప్రత్యక్ష';
+$lang['reports_range_invalid'] = 'రేంజ్ చెల్లదు.';
+$lang['reports_report_data'] = 'నివేదిక డేటా';
+$lang['reports_report_settings'] = 'నివేదిక సెట్టింగులు';
+$lang['reports_reports'] = 'నివేదికలు';
+$lang['reports_today'] = 'నేడు';
+$lang['reports_top_device_types'] = 'టాప్ పరికర రకాలు';
+$lang['reports_top_external_ips'] = 'టాప్ బాహ్య IP లు';
+$lang['reports_top_ips'] = 'టాప్ IP లు';
+$lang['reports_top_users'] = 'అగ్ర యూజర్లు';
+$lang['reports_user_summary'] = 'వాడుకరి సారాంశం';
+$lang['reports_yesterday'] = 'నిన్న';
