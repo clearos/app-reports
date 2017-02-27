c9bdc8a9d3533db296f99d07d71f6f76f4932041
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:41:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = '该基地报告应用程序提供操作系统一套标准的报告工具。';
+$lang['reports_app_name'] = '基地报告';
+$lang['reports_date_range'] = '日期范围';
+$lang['reports_device_type_summary'] = '设备类型摘要';
+$lang['reports_device_types'] = '设备类型';
+$lang['reports_entries'] = '参赛作品';
+$lang['reports_full_report'] = '报告全文';
+$lang['reports_ip_summary'] = 'IP摘要';
+$lang['reports_last_30_days'] = '最近30天';
+$lang['reports_last_7_days'] = '过去7天';
+$lang['reports_live'] = '生活';
+$lang['reports_range_invalid'] = '范围无效。';
+$lang['reports_report_data'] = '报告数据';
+$lang['reports_report_settings'] = '报告设置';
+$lang['reports_reports'] = '报告';
+$lang['reports_today'] = '今天';
+$lang['reports_top_device_types'] = '顶级设备类型';
+$lang['reports_top_external_ips'] = '顶部外部IP地址';
+$lang['reports_top_ips'] = '顶级的IP';
+$lang['reports_top_users'] = '用户排行';
+$lang['reports_user_summary'] = '用户摘要';
+$lang['reports_yesterday'] = '昨天';
