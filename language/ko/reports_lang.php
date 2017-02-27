829e6ae34dbc3795bc47f88a94c8d21754db42f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:45:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = '베이스 리포트 응용 프로그램은 운영 체제에보고 표준 툴 세트를 제공한다.';
+$lang['reports_app_name'] = '자료 보고서';
+$lang['reports_date_range'] = '날짜 범위';
+$lang['reports_device_type_summary'] = '장치 유형 요약';
+$lang['reports_device_types'] = '장치 유형';
+$lang['reports_entries'] = '항목';
+$lang['reports_full_report'] = '전체 보고서';
+$lang['reports_ip_summary'] = 'IP 요약';
+$lang['reports_last_30_days'] = '지난 30 일';
+$lang['reports_last_7_days'] = '지난 7 일';
+$lang['reports_live'] = '살고 있다';
+$lang['reports_range_invalid'] = '범위가 잘못되었습니다.';
+$lang['reports_report_data'] = '보고서의 데이터';
+$lang['reports_report_settings'] = '보고서 설정';
+$lang['reports_reports'] = '보고서';
+$lang['reports_today'] = '오늘';
+$lang['reports_top_device_types'] = '상위 장치 유형';
+$lang['reports_top_external_ips'] = '탑 외부 IP를';
+$lang['reports_top_ips'] = '인기 IP를';
+$lang['reports_top_users'] = '최고 사용자';
+$lang['reports_user_summary'] = '사용자 요약';
+$lang['reports_yesterday'] = '어제';
