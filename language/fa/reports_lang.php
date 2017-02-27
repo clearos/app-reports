90acc1c27f3cafa5968ed04e34f53f1bcd948f33
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:47:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'برنامه گزارش پایگاه مجموعه ای از ابزار گزارش استاندارد برای سیستم عامل فراهم می کند.';
+$lang['reports_app_name'] = 'گزارش پایه';
+$lang['reports_date_range'] = 'محدوده زمانی';
+$lang['reports_device_type_summary'] = 'دستگاه خلاصه نوع';
+$lang['reports_device_types'] = 'انواع دستگاه های';
+$lang['reports_entries'] = 'ورودی های';
+$lang['reports_full_report'] = 'گزارش کامل';
+$lang['reports_ip_summary'] = 'IP خلاصه';
+$lang['reports_last_30_days'] = 'گذشته 30 روز گذشته';
+$lang['reports_last_7_days'] = 'در 7 روز گذشته';
+$lang['reports_live'] = 'زنده';
+$lang['reports_range_invalid'] = 'محدوده نامعتبر است.';
+$lang['reports_report_data'] = 'گزارش داده';
+$lang['reports_report_settings'] = 'تنظیمات را گزارش دهید';
+$lang['reports_reports'] = 'گزارش ها';
+$lang['reports_today'] = 'امروز';
+$lang['reports_top_device_types'] = 'انواع دستگاه بالا';
+$lang['reports_top_external_ips'] = 'بالا IP های خارجی';
+$lang['reports_top_ips'] = 'IP های بالا';
+$lang['reports_top_users'] = 'کاربران برتر';
+$lang['reports_user_summary'] = 'کاربر خلاصه';
+$lang['reports_yesterday'] = 'دیروز';
