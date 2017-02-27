a561d4fdf66b1eef03782f418aa9f928a208c33f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:47:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'आधार रिपोर्टहरू अनुप्रयोग सञ्चालन प्रणाली लागि मानक रिपोर्ट उपकरण को एक सेट प्रदान गर्दछ।';
+$lang['reports_app_name'] = 'आधार प्रतिवेदन';
+$lang['reports_date_range'] = 'मिति सीमा';
+$lang['reports_device_type_summary'] = 'उपकरण प्रकार सारांश';
+$lang['reports_device_types'] = 'उपकरण प्रकार';
+$lang['reports_entries'] = 'प्रविष्टिहरू';
+$lang['reports_full_report'] = 'पूर्ण रिपोर्ट';
+$lang['reports_ip_summary'] = 'आईपी ​​सारांश';
+$lang['reports_last_30_days'] = 'पछिल्लो 30 दिन';
+$lang['reports_last_7_days'] = 'पछिल्लो 7 दिन';
+$lang['reports_live'] = 'प्रत्यक्ष';
+$lang['reports_range_invalid'] = 'सीमा अमान्य छ।';
+$lang['reports_report_data'] = 'रिपोर्ट डाटा';
+$lang['reports_report_settings'] = 'रिपोर्ट सेटिङ';
+$lang['reports_reports'] = 'रिपोर्ट';
+$lang['reports_today'] = 'आज';
+$lang['reports_top_device_types'] = 'शीर्ष उपकरण प्रकार';
+$lang['reports_top_external_ips'] = 'शीर्ष बाह्य IP हरू';
+$lang['reports_top_ips'] = 'शीर्ष IP हरू';
+$lang['reports_top_users'] = 'शीर्ष प्रयोगकर्ता';
+$lang['reports_user_summary'] = 'प्रयोगकर्ता सारांश';
+$lang['reports_yesterday'] = 'हिजो';
