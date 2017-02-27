2393385012202f9a3c9317e2b4256c00d0445bc8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:49:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'The app e Reports Base fana ka sete ya disebediswa maemo a itlalehe teng bakeng sa tsamaiso ya ho sebetsa.';
+$lang['reports_app_name'] = 'Reports Base';
+$lang['reports_date_range'] = 'letsatsi Range';
+$lang['reports_device_type_summary'] = 'Type sesebediswa Summary';
+$lang['reports_device_types'] = 'sesebediswa Types';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Report Full';
+$lang['reports_ip_summary'] = 'IP Summary';
+$lang['reports_last_30_days'] = 'Matsatsi a ho Qetela 30';
+$lang['reports_last_7_days'] = 'Last 7 Matsatsi';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range ha o sebetse.';
+$lang['reports_report_data'] = 'Report Data';
+$lang['reports_report_settings'] = 'Report Settings';
+$lang['reports_reports'] = 'Reports';
+$lang['reports_today'] = 'kajeno';
+$lang['reports_top_device_types'] = 'Top mochine Types';
+$lang['reports_top_external_ips'] = 'Top External IPs';
+$lang['reports_top_ips'] = 'Top IPs';
+$lang['reports_top_users'] = 'Top Users';
+$lang['reports_user_summary'] = 'Summary User';
+$lang['reports_yesterday'] = 'maobane';
