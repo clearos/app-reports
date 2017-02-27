f4f9258af7650641ad2d3c8f5a5755a1071239d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:52:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Uhlelo lokusebenza Imibiko Base inikeza iqoqo standard umbiko amathuluzi for uhlelo lokusebenza.';
+$lang['reports_app_name'] = 'Imibiko Base';
+$lang['reports_date_range'] = 'Usuku Range';
+$lang['reports_device_type_summary'] = 'Uhlobo Kudivayisi Summary';
+$lang['reports_device_types'] = 'Izinhlobo Kudivayisi';
+$lang['reports_entries'] = 'Umncintiswano';
+$lang['reports_full_report'] = 'Umbiko Full';
+$lang['reports_ip_summary'] = 'IP Summary';
+$lang['reports_last_30_days'] = 'Izinsuku ezingu-30 zokugcina';
+$lang['reports_last_7_days'] = '7 Ezinsukwini Zokugcina';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range ayivumelekile.';
+$lang['reports_report_data'] = 'Umbiko Data';
+$lang['reports_report_settings'] = 'Umbiko Izilungiselelo';
+$lang['reports_reports'] = 'Imibiko';
+$lang['reports_today'] = 'Namuhla';
+$lang['reports_top_device_types'] = 'Izinhlobo Kudivayisi Top';
+$lang['reports_top_external_ips'] = 'Top IPS Lwangaphandle';
+$lang['reports_top_ips'] = 'PS Top';
+$lang['reports_top_users'] = 'Abasebenzisi Top';
+$lang['reports_user_summary'] = 'Isifinyezo Umsebenzisi';
+$lang['reports_yesterday'] = 'Izolo';
