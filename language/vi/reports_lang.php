4073cca9d2b3ad50c4865ee07d423cb773d01722
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:51:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Ứng dụng Báo cáo cơ sở cung cấp một bộ công cụ báo cáo chuẩn cho hệ điều hành.';
+$lang['reports_app_name'] = 'Báo cáo cơ sở';
+$lang['reports_date_range'] = 'Phạm vi ngày';
+$lang['reports_device_type_summary'] = 'Loại thiết bị Tóm tắt';
+$lang['reports_device_types'] = 'Các loại thiết bị';
+$lang['reports_entries'] = 'Mục';
+$lang['reports_full_report'] = 'báo cáo đầy đủ';
+$lang['reports_ip_summary'] = 'Tóm tắt IP';
+$lang['reports_last_30_days'] = '30 ngày trước';
+$lang['reports_last_7_days'] = '7 ngày qua';
+$lang['reports_live'] = 'Trực tiếp';
+$lang['reports_range_invalid'] = 'Range là không hợp lệ.';
+$lang['reports_report_data'] = 'báo cáo số liệu';
+$lang['reports_report_settings'] = 'Cài đặt báo cáo';
+$lang['reports_reports'] = 'Báo cáo';
+$lang['reports_today'] = 'Hôm nay';
+$lang['reports_top_device_types'] = 'Các loại thiết bị hàng đầu';
+$lang['reports_top_external_ips'] = 'Lên trên ngoài các khu công nghiệp';
+$lang['reports_top_ips'] = 'Lên trên IP';
+$lang['reports_top_users'] = 'Về đầu Trang';
+$lang['reports_user_summary'] = 'Tóm tắt tài';
+$lang['reports_yesterday'] = 'Hôm qua';
