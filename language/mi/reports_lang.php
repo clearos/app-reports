1cad81aae5bedd5d7c324e9233803f38c3abe721
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:46:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Te taupānga Base Pūrongo whakarato te huinga o ngā taputapu pūrongo paerewa mō te pūnaha whakahaere.';
+$lang['reports_app_name'] = 'pūrongo Base';
+$lang['reports_date_range'] = 'Awhe rā';
+$lang['reports_device_type_summary'] = 'Momo Pūrere Whakarāpopototanga';
+$lang['reports_device_types'] = 'momo pūrere';
+$lang['reports_entries'] = 'tāurunga';
+$lang['reports_full_report'] = 'Pūrongo tonu';
+$lang['reports_ip_summary'] = 'Whakarāpopototanga IP';
+$lang['reports_last_30_days'] = 'Last 30 Days';
+$lang['reports_last_7_days'] = 'Last 7 Days';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'He muhu Range.';
+$lang['reports_report_data'] = 'Raraunga Report';
+$lang['reports_report_settings'] = 'Tautuhinga Report';
+$lang['reports_reports'] = 'pūrongo';
+$lang['reports_today'] = 'teie mahana';
+$lang['reports_top_device_types'] = 'Momo Pūrere Top';
+$lang['reports_top_external_ips'] = 'Top waho IP';
+$lang['reports_top_ips'] = 'IP Top';
+$lang['reports_top_users'] = 'Kaiwhakamahi Top';
+$lang['reports_user_summary'] = 'Whakarāpopoto kaiwhakamahi';
+$lang['reports_yesterday'] = 'inanahi';
