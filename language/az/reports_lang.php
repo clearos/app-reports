6ffec08914036769cb14e069a0fe20183f0051d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:39:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Base Hesabatlar app əməliyyat sistemi üçün standart hesabat alətlər bir sıra təmin edir.';
+$lang['reports_app_name'] = 'Base Hesabatlar';
+$lang['reports_date_range'] = 'Tarix Aralığı';
+$lang['reports_device_type_summary'] = 'Device Tipi Xülasə';
+$lang['reports_device_types'] = 'Device növləri';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Tam Report';
+$lang['reports_ip_summary'] = 'IP Xülasə';
+$lang['reports_last_30_days'] = 'Son 30 Gün';
+$lang['reports_last_7_days'] = 'Son 7 Days';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range yanlışdır.';
+$lang['reports_report_data'] = 'Report Data';
+$lang['reports_report_settings'] = 'Report Settings';
+$lang['reports_reports'] = 'Hesabatlar';
+$lang['reports_today'] = 'hazırda';
+$lang['reports_top_device_types'] = 'Top Device növləri';
+$lang['reports_top_external_ips'] = 'Top Xarici IP';
+$lang['reports_top_ips'] = 'Top IP';
+$lang['reports_top_users'] = 'Top İstifadəçilər';
+$lang['reports_user_summary'] = 'İstifadəçi Xülasə';
+$lang['reports_yesterday'] = 'dünən';
