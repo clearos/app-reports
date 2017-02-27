57482df6672c7072b8f9b78e76d5f3373f5c6549
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:49:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'The app Wararka Base bixisaa set oo ah qalabka warbixinta caadiga ah nidaamka qalliinka.';
+$lang['reports_app_name'] = 'Wararka Base';
+$lang['reports_date_range'] = 'Taariikhda Range';
+$lang['reports_device_type_summary'] = 'Nooca Device Kooban';
+$lang['reports_device_types'] = 'Noocyada Device';
+$lang['reports_entries'] = 'entries';
+$lang['reports_full_report'] = 'Warbixinta oo dhameystiran';
+$lang['reports_ip_summary'] = 'IP Kooban';
+$lang['reports_last_30_days'] = 'Last 30 Maalmood';
+$lang['reports_last_7_days'] = 'Last 7 Days';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range waa baadil.';
+$lang['reports_report_data'] = 'Data Report';
+$lang['reports_report_settings'] = 'Settings Report';
+$lang['reports_reports'] = 'Wararka';
+$lang['reports_today'] = 'Maanta';
+$lang['reports_top_device_types'] = 'Noocyada Device Top';
+$lang['reports_top_external_ips'] = 'Top Dibadda IPS';
+$lang['reports_top_ips'] = 'IPS Top';
+$lang['reports_top_users'] = 'Users Top';
+$lang['reports_user_summary'] = 'User Kooban';
+$lang['reports_yesterday'] = 'Yesterday';
