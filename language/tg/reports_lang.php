f22fd56f8aacaadc2278c2cfcaabb8b8756a39d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:50:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Дар барнома Ҳисобот Пойгоҳи маҷмӯи воситаҳои гузориши стандартии системаи пардозандаро дар ин таъмин менамояд.';
+$lang['reports_app_name'] = 'Ҳисоботҳо Пойгоҳи';
+$lang['reports_date_range'] = 'Санаи Диапазони';
+$lang['reports_device_type_summary'] = 'Дастгоҳ хулоса навъи';
+$lang['reports_device_types'] = 'Намудҳои дастгоҳро';
+$lang['reports_entries'] = 'вурудоти';
+$lang['reports_full_report'] = 'Њисобот оид ба пурра';
+$lang['reports_ip_summary'] = 'IP мухтасари';
+$lang['reports_last_30_days'] = '30 рӯзи охир';
+$lang['reports_last_7_days'] = 'Last 7 рӯз';
+$lang['reports_live'] = 'Зиндагӣ';
+$lang['reports_range_invalid'] = 'Диапазони нодуруст аст.';
+$lang['reports_report_data'] = 'Њисобот оид ба маълумот';
+$lang['reports_report_settings'] = 'Танзимот Њисобот оид ба';
+$lang['reports_reports'] = 'ҳисоботҳо';
+$lang['reports_today'] = 'имрӯз';
+$lang['reports_top_device_types'] = 'Намудҳои дастгоҳро Top';
+$lang['reports_top_external_ips'] = 'Top IPS беруна';
+$lang['reports_top_ips'] = 'IPS Top';
+$lang['reports_top_users'] = 'Истифодабарандагон дар Top';
+$lang['reports_user_summary'] = 'Мазмуни мухтасари корбар';
+$lang['reports_yesterday'] = 'дирӯз';
