1c0e611428bfaaf2811af051344343d0a9d2fb1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:43:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'The Base Reports ngwa na-enye a set nke ọkọlọtọ akụkọ ngwaọrụ n&#39;ihi na sistemụ arụmọrụ.';
+$lang['reports_app_name'] = 'Base Reports';
+$lang['reports_date_range'] = 'Ụbọchị iche';
+$lang['reports_device_type_summary'] = 'Ngwaọrụ Type Summary';
+$lang['reports_device_types'] = 'ngwaọrụ Ụdị';
+$lang['reports_entries'] = 'Ndenye';
+$lang['reports_full_report'] = 'Full Report';
+$lang['reports_ip_summary'] = 'IP Summary';
+$lang['reports_last_30_days'] = 'Ikpeazụ 30 Days';
+$lang['reports_last_7_days'] = 'Ikpeazụ 7 Days';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Nso bụ ghara ịdị irè.';
+$lang['reports_report_data'] = 'Report Data';
+$lang['reports_report_settings'] = 'Report Settings';
+$lang['reports_reports'] = 'Reports';
+$lang['reports_today'] = 'taa';
+$lang['reports_top_device_types'] = 'Top Device Ụdị';
+$lang['reports_top_external_ips'] = 'Top Mpụga IPS';
+$lang['reports_top_ips'] = 'top IPS';
+$lang['reports_top_users'] = 'top Ọrụ';
+$lang['reports_user_summary'] = 'User Summary';
+$lang['reports_yesterday'] = 'ụnyaahụ';
