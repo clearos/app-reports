27cdad068031e2e7a14773cf0cbd3a354d1b30d5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:51:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'بیس رپورٹیں اے پی پی آپریٹنگ سسٹم کے لئے معیاری رپورٹ اوزار کا ایک سیٹ فراہم کرتا ہے.';
+$lang['reports_app_name'] = 'بیس کی رپورٹیں';
+$lang['reports_date_range'] = 'تاریخ کی حد';
+$lang['reports_device_type_summary'] = 'ڈیوائس کی قسم کا خلاصہ';
+$lang['reports_device_types'] = 'ڈیوائس کی اقسا م';
+$lang['reports_entries'] = 'اندراجات';
+$lang['reports_full_report'] = 'مکمل رپورٹ';
+$lang['reports_ip_summary'] = 'IP خلاصہ';
+$lang['reports_last_30_days'] = 'گزشتہ 30 دنوں';
+$lang['reports_last_7_days'] = 'گزشتہ 7 دن';
+$lang['reports_live'] = 'لائیو';
+$lang['reports_range_invalid'] = 'حد باطل ہے.';
+$lang['reports_report_data'] = 'رپورٹ ڈیٹا';
+$lang['reports_report_settings'] = 'رپورٹ ترتیبات';
+$lang['reports_reports'] = 'رپورٹس';
+$lang['reports_today'] = 'آج';
+$lang['reports_top_device_types'] = 'مقبول ڈیوائس کی اقسا م';
+$lang['reports_top_external_ips'] = 'اوپر خارجی آئی پی ایس';
+$lang['reports_top_ips'] = 'اوپر آئی پی ایس';
+$lang['reports_top_users'] = 'اوپر صارفین';
+$lang['reports_user_summary'] = 'رکن کا خلاصہ';
+$lang['reports_yesterday'] = 'گزشتہ کل';
