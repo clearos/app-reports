87de31cc04c828ce0e91d0728a12c1879dd8c41a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:44:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Негізгі бағдарлама операциялық жүйесі үшін стандартты есеп құралдарының жиынтығын ұсынады Есептер.';
+$lang['reports_app_name'] = 'Негізгі Есептер';
+$lang['reports_date_range'] = 'күні Range';
+$lang['reports_device_type_summary'] = 'Құрылғы түрі Жиынтық';
+$lang['reports_device_types'] = 'құрылғы түрлері';
+$lang['reports_entries'] = 'жазбалар';
+$lang['reports_full_report'] = 'Толық есеп';
+$lang['reports_ip_summary'] = 'IP Резюме';
+$lang['reports_last_30_days'] = 'Соңғы 30 күнде';
+$lang['reports_last_7_days'] = 'Соңғы 7 күн';
+$lang['reports_live'] = 'тірі';
+$lang['reports_range_invalid'] = 'Range жарамсыз болып табылады.';
+$lang['reports_report_data'] = 'есеп деректерін';
+$lang['reports_report_settings'] = 'есебі параметрлерін';
+$lang['reports_reports'] = 'Есептер';
+$lang['reports_today'] = 'бүгін';
+$lang['reports_top_device_types'] = 'Жоғары құрылғы түрлерін';
+$lang['reports_top_external_ips'] = 'Жоғары Сыртқы IPs';
+$lang['reports_top_ips'] = 'Жоғары IPs';
+$lang['reports_top_users'] = 'Жоғары Пайдаланушылар';
+$lang['reports_user_summary'] = 'пайдаланушы Резюме';
+$lang['reports_yesterday'] = 'кеше';
