dafd164eda10dd05f6b294b469d02838de9b0eca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:40:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'বেজ রিপোর্ট অ্যাপ্লিকেশন অপারেটিং সিস্টেমের জন্য মান রিপোর্ট সরঞ্জামের একটি সেট প্রদান করে.';
+$lang['reports_app_name'] = 'বেজ রিপোর্ট';
+$lang['reports_date_range'] = 'তারিখের পরিসীমা';
+$lang['reports_device_type_summary'] = 'ডিভাইস প্রকার সংক্ষিপ্তসার';
+$lang['reports_device_types'] = 'ডিভাইস প্রকারভেদ';
+$lang['reports_entries'] = 'দাখিলা';
+$lang['reports_full_report'] = 'সম্পূর্ণ রিপোর্ট';
+$lang['reports_ip_summary'] = 'আইপি সংক্ষিপ্তসার';
+$lang['reports_last_30_days'] = 'শেষ 30 দিন';
+$lang['reports_last_7_days'] = 'শেষ 7 দিনের';
+$lang['reports_live'] = 'লাইভ';
+$lang['reports_range_invalid'] = 'বিন্যাস অবৈধ.';
+$lang['reports_report_data'] = 'গালাগাল প্রতিবেদন ডেটা';
+$lang['reports_report_settings'] = 'গালাগাল প্রতিবেদন সেটিংস';
+$lang['reports_reports'] = 'প্রতিবেদন';
+$lang['reports_today'] = 'আজ';
+$lang['reports_top_device_types'] = 'শীর্ষ ডিভাইস প্রকারভেদ';
+$lang['reports_top_external_ips'] = 'শীর্ষ এক্সটার্নাল আইপিগুলি';
+$lang['reports_top_ips'] = 'শীর্ষ আইপিগুলি';
+$lang['reports_top_users'] = 'প্রজন্ম ডট কম';
+$lang['reports_user_summary'] = 'ব্যবহারকারী সংক্ষিপ্তসার';
+$lang['reports_yesterday'] = 'গতকাল';
