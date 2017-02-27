ab62cbfef05e7cb908e1c796c5e873822e091187
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:39:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Die Base Verslae app bied &#39;n stel standaard verslag gereedskap vir die bedryfstelsel.';
+$lang['reports_app_name'] = 'Base Verslae';
+$lang['reports_date_range'] = 'datum Range';
+$lang['reports_device_type_summary'] = 'Type apparaat Opsomming';
+$lang['reports_device_types'] = 'toestel tipes';
+$lang['reports_entries'] = 'inskrywings';
+$lang['reports_full_report'] = 'volledige verslag';
+$lang['reports_ip_summary'] = 'IP Opsomming';
+$lang['reports_last_30_days'] = 'Laaste 30 Dae';
+$lang['reports_last_7_days'] = 'Afgelope 7 dae';
+$lang['reports_live'] = 'Leef';
+$lang['reports_range_invalid'] = 'Range is ongeldig.';
+$lang['reports_report_data'] = 'verslag Data';
+$lang['reports_report_settings'] = 'Verslagkodes';
+$lang['reports_reports'] = 'Berigte';
+$lang['reports_today'] = 'vandag';
+$lang['reports_top_device_types'] = 'Top Device tipes';
+$lang['reports_top_external_ips'] = 'Top Eksterne IPs';
+$lang['reports_top_ips'] = 'Top IPs';
+$lang['reports_top_users'] = 'Top Gebruikers';
+$lang['reports_user_summary'] = 'gebruikers Opsomming';
+$lang['reports_yesterday'] = 'gister';
