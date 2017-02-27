5f6a4486e482f0b85f3bd7b95194dd68fc12bf07
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:47:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'ਬੇਸ ਰਿਪੋਰਟ ਐਪ ਓਪਰੇਟਿੰਗ ਸਿਸਟਮ ਲਈ ਮਿਆਰੀ ਰਿਪੋਰਟ ਨੂੰ ਸੰਦ ਦਾ ਇੱਕ ਸੈੱਟ ਦਿੰਦਾ ਹੈ.';
+$lang['reports_app_name'] = 'ਬੇਸ ਰਿਪੋਰਟ';
+$lang['reports_date_range'] = 'ਮਿਤੀ ਸੀਮਾ';
+$lang['reports_device_type_summary'] = 'ਜੰਤਰ ਕਿਸਮ ਸੰਖੇਪ';
+$lang['reports_device_types'] = 'ਜੰਤਰ ਕਿਸਮ';
+$lang['reports_entries'] = 'ਇੰਦਰਾਜ਼';
+$lang['reports_full_report'] = 'ਪੂਰੀ ਰਿਪੋਰਟ';
+$lang['reports_ip_summary'] = 'IP ਨੂੰ ਸੰਖੇਪ';
+$lang['reports_last_30_days'] = 'ਪਿਛਲੇ 30 ਦਿਨ';
+$lang['reports_last_7_days'] = 'ਆਖਰੀ 7 ਦਿਨ';
+$lang['reports_live'] = 'ਲਾਈਵ';
+$lang['reports_range_invalid'] = 'ਸੀਮਾ ਗਲਤ ਹੈ.';
+$lang['reports_report_data'] = 'ਦੀ ਰਿਪੋਰਟ ਡਾਟਾ';
+$lang['reports_report_settings'] = 'ਦੀ ਰਿਪੋਰਟ ਸੈਟਿੰਗ';
+$lang['reports_reports'] = 'ਰਿਪੋਰਟ';
+$lang['reports_today'] = 'ਅੱਜ';
+$lang['reports_top_device_types'] = 'ਸਿਖਰ ਜੰਤਰ ਕਿਸਮ';
+$lang['reports_top_external_ips'] = 'ਸਿਖਰ ਬਾਹਰੀ IPs';
+$lang['reports_top_ips'] = 'ਸਿਖਰ IPs';
+$lang['reports_top_users'] = 'ਸਿਖਰ ਉਪਭੋਗਤਾ';
+$lang['reports_user_summary'] = 'ਯੂਜ਼ਰ ਸੰਖੇਪ';
+$lang['reports_yesterday'] = 'ਕੱਲ੍ਹ';
