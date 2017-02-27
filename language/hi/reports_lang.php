245cddcd50bbbab0b2412678f7f48b008928e711
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:43:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'बेस रिपोर्ट एप्लिकेशन ऑपरेटिंग सिस्टम के लिए मानक रिपोर्ट उपकरणों का एक सेट प्रदान करता है।';
+$lang['reports_app_name'] = 'बेस रिपोर्ट';
+$lang['reports_date_range'] = 'तिथि सीमा';
+$lang['reports_device_type_summary'] = 'डिवाइस प्रकार सारांश';
+$lang['reports_device_types'] = 'डिवाइस प्रकार';
+$lang['reports_entries'] = 'प्रविष्टियां';
+$lang['reports_full_report'] = 'पूरी रिपोर्ट';
+$lang['reports_ip_summary'] = 'आईपी ​​सारांश';
+$lang['reports_last_30_days'] = 'पिछले 30 दिनों में';
+$lang['reports_last_7_days'] = 'पिछले 7 दिन';
+$lang['reports_live'] = 'जीना';
+$lang['reports_range_invalid'] = 'रेंज अमान्य है।';
+$lang['reports_report_data'] = 'रिपोर्ट डेटा';
+$lang['reports_report_settings'] = 'रिपोर्ट सेटिंग';
+$lang['reports_reports'] = 'रिपोर्ट';
+$lang['reports_today'] = 'आज';
+$lang['reports_top_device_types'] = 'शीर्ष डिवाइस प्रकार';
+$lang['reports_top_external_ips'] = 'शीर्ष बाहरी आईपी';
+$lang['reports_top_ips'] = 'शीर्ष आईपीएस';
+$lang['reports_top_users'] = 'शीर्ष उपयोगकर्ता';
+$lang['reports_user_summary'] = 'उपयोगकर्ता सारांश';
+$lang['reports_yesterday'] = 'बिता कल';
