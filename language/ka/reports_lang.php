e375a079d80418473532b93d7e5ecccc15dbaa37
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:44:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'ბაზის ანგარიშები app უზრუნველყოფს კომპლექტი სტანდარტული ანგარიში ინსტრუმენტები ოპერაციული სისტემა.';
+$lang['reports_app_name'] = 'ბაზის ანგარიშები';
+$lang['reports_date_range'] = 'თარიღი დიაპაზონი';
+$lang['reports_device_type_summary'] = 'მოწყობილობის ტიპი რეზიუმე';
+$lang['reports_device_types'] = 'მოწყობილობა სახეები';
+$lang['reports_entries'] = 'შესვლის';
+$lang['reports_full_report'] = 'სრული ანგარიში';
+$lang['reports_ip_summary'] = 'IP რეზიუმე';
+$lang['reports_last_30_days'] = 'ბოლო 30 დღე';
+$lang['reports_last_7_days'] = 'ბოლო 7 დღე';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range არასწორია.';
+$lang['reports_report_data'] = 'ანგარიშის მონაცემები';
+$lang['reports_report_settings'] = 'ანგარიშის პარამეტრები';
+$lang['reports_reports'] = 'ანგარიშები';
+$lang['reports_today'] = 'დღეს';
+$lang['reports_top_device_types'] = 'ყველაზე მოწყობილობა სახეები';
+$lang['reports_top_external_ips'] = 'ყველაზე გარე IP მისამართები';
+$lang['reports_top_ips'] = 'ყველაზე IP მისამართები';
+$lang['reports_top_users'] = 'TOP მომხმარებლები';
+$lang['reports_user_summary'] = 'მომხმარებელი რეზიუმე';
+$lang['reports_yesterday'] = 'გუშინ';
