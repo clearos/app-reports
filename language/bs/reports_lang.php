d18ba49033a44f7d8cfef13db5cf213685bc04e1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:40:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Bazi Izvještaji aplikacija pruža niz standardnih alata izvještaja za operativni sistem.';
+$lang['reports_app_name'] = 'Base Izvještaji';
+$lang['reports_date_range'] = 'Datum Range';
+$lang['reports_device_type_summary'] = 'Tip uređaja Pregled';
+$lang['reports_device_types'] = 'Vrste uređaja';
+$lang['reports_entries'] = 'Unose';
+$lang['reports_full_report'] = 'Cijeli izvještaj';
+$lang['reports_ip_summary'] = 'IP Pregled';
+$lang['reports_last_30_days'] = 'Zadnjih 30 dana';
+$lang['reports_last_7_days'] = 'Zadnjih 7 dana';
+$lang['reports_live'] = 'live';
+$lang['reports_range_invalid'] = 'Opseg je nevažeći.';
+$lang['reports_report_data'] = 'izvještaj podataka';
+$lang['reports_report_settings'] = 'izvještaj Postavke';
+$lang['reports_reports'] = 'Izvještaji';
+$lang['reports_today'] = 'danas';
+$lang['reports_top_device_types'] = 'Top Vrste uređaja';
+$lang['reports_top_external_ips'] = 'Top Vanjske IP';
+$lang['reports_top_ips'] = 'Top IP';
+$lang['reports_top_users'] = 'Top korisnici';
+$lang['reports_user_summary'] = 'korisnik Pregled';
+$lang['reports_yesterday'] = 'juče';
