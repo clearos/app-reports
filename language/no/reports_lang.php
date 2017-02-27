2ab78d8c6b8146b9d4a0573e6dc102fcfe987d8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:47:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'The Base Reports app gir et sett med standard rapportverktøy for operativsystemet.';
+$lang['reports_app_name'] = 'Base Reports';
+$lang['reports_date_range'] = 'Datointervall';
+$lang['reports_device_type_summary'] = 'Enhetstype Oppsummering';
+$lang['reports_device_types'] = 'enhets~~POS=TRUNC typer~~POS=HEADCOMP';
+$lang['reports_entries'] = 'Innganger';
+$lang['reports_full_report'] = 'Full Rapporter';
+$lang['reports_ip_summary'] = 'IP-sammendrag';
+$lang['reports_last_30_days'] = 'Siste 30 dager';
+$lang['reports_last_7_days'] = 'Siste 7 dager';
+$lang['reports_live'] = 'Bo';
+$lang['reports_range_invalid'] = 'Range er ugyldig.';
+$lang['reports_report_data'] = 'rapportdata';
+$lang['reports_report_settings'] = 'rapport~~POS=TRUNC innstillinger~~POS=HEADCOMP';
+$lang['reports_reports'] = 'Reports';
+$lang['reports_today'] = 'I dag';
+$lang['reports_top_device_types'] = 'Topp enhetstyper';
+$lang['reports_top_external_ips'] = 'Top Ekstern IP-adresser';
+$lang['reports_top_ips'] = 'Topp IPer';
+$lang['reports_top_users'] = 'Topp brukere';
+$lang['reports_user_summary'] = 'Bruker oppsummering';
+$lang['reports_yesterday'] = 'I går';
