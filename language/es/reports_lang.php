627884f9ef95869fe19b16fb5ba557cab86832ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:49:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'La aplicación Informes Base proporciona un conjunto de herramientas de informes estándar para el sistema operativo.';
+$lang['reports_app_name'] = 'Los informes de base';
+$lang['reports_date_range'] = 'Rango de fechas';
+$lang['reports_device_type_summary'] = 'Tipo de dispositivo Resumen';
+$lang['reports_device_types'] = 'Tipos de dispositivos';
+$lang['reports_entries'] = 'entradas';
+$lang['reports_full_report'] = 'Reporte completo';
+$lang['reports_ip_summary'] = 'Resumen de IP';
+$lang['reports_last_30_days'] = 'Últimos 30 días';
+$lang['reports_last_7_days'] = 'Los últimos 7 días';
+$lang['reports_live'] = 'Vivir';
+$lang['reports_range_invalid'] = 'El rango es válido.';
+$lang['reports_report_data'] = 'datos del informe';
+$lang['reports_report_settings'] = 'Configuración de informe';
+$lang['reports_reports'] = 'Informes';
+$lang['reports_today'] = 'Hoy';
+$lang['reports_top_device_types'] = 'Tipos de dispositivos Top';
+$lang['reports_top_external_ips'] = 'Top IP externa';
+$lang['reports_top_ips'] = 'Top IPs';
+$lang['reports_top_users'] = 'Top usuarios';
+$lang['reports_user_summary'] = 'Resumen del usuario';
+$lang['reports_yesterday'] = 'Ayer';
