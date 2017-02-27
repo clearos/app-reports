c48a1fd4411c44cbfd38b1235fd18708c18bcec6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:44:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Grunnurinn Skýrslur app veitir stöðluð verkfæri skýrslu fyrir stýrikerfi.';
+$lang['reports_app_name'] = 'Base Skýrslur';
+$lang['reports_date_range'] = 'Tímabil';
+$lang['reports_device_type_summary'] = 'Tæki Tegund Yfirlit';
+$lang['reports_device_types'] = 'tæki Tegundir';
+$lang['reports_entries'] = 'færslur';
+$lang['reports_full_report'] = 'Full Report';
+$lang['reports_ip_summary'] = 'IP Yfirlit';
+$lang['reports_last_30_days'] = 'Síðustu 30 dagar';
+$lang['reports_last_7_days'] = 'Síðustu 7 dagar';
+$lang['reports_live'] = 'Lifa';
+$lang['reports_range_invalid'] = 'Range er ógilt.';
+$lang['reports_report_data'] = 'skýrsla Data';
+$lang['reports_report_settings'] = 'Tilkynna Stillingar';
+$lang['reports_reports'] = 'skýrslur';
+$lang['reports_today'] = 'í dag';
+$lang['reports_top_device_types'] = 'Helstu tegundir tækja';
+$lang['reports_top_external_ips'] = 'Top External IP-tölum';
+$lang['reports_top_ips'] = 'Top IP-tölum';
+$lang['reports_top_users'] = 'Helstu notendur';
+$lang['reports_user_summary'] = 'User Yfirlit';
+$lang['reports_yesterday'] = 'Í gær';
