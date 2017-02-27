a33cd95235561d4bd5a9522cb559261d00ecf776
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:45:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'ການ app ບົດລາຍງານຖານໃຫ້ຊຸດເຄື່ອງມືບົດລາຍງານມາດຕະຖານສໍາລັບລະບົບປະຕິບັດການ.';
+$lang['reports_app_name'] = 'ບົດລາຍງານພື້ນຖານ';
+$lang['reports_date_range'] = 'Range ວັນທີ່';
+$lang['reports_device_type_summary'] = 'ປະເພດອຸປະກອນ Summary';
+$lang['reports_device_types'] = 'ປະເພດອຸປະກອນ';
+$lang['reports_entries'] = 'ການອອກສຽງ';
+$lang['reports_full_report'] = 'ບົດລາຍງານເຕັມ';
+$lang['reports_ip_summary'] = 'IP Summary';
+$lang['reports_last_30_days'] = '30 ມື້ສຸດທ້າຍ';
+$lang['reports_last_7_days'] = '7 ວັນ';
+$lang['reports_live'] = 'ດໍາລົງຊີວິດ';
+$lang['reports_range_invalid'] = 'ລະດັບຄວາມແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['reports_report_data'] = 'ຂໍ້ມູນບົດລາຍງານ';
+$lang['reports_report_settings'] = 'ການຕັ້ງຄ່າລາຍງານ';
+$lang['reports_reports'] = 'ບົດລາຍງານ';
+$lang['reports_today'] = 'ໃນມື້ນີ້';
+$lang['reports_top_device_types'] = 'ປະເພດອຸປະກອນທາງເທີງ';
+$lang['reports_top_external_ips'] = 'IPs External Top';
+$lang['reports_top_ips'] = 'IPs Top';
+$lang['reports_top_users'] = 'ຂ້າງເທິງຜູ້ທີ່';
+$lang['reports_user_summary'] = 'Summary ຜູ້ໃຊ້';
+$lang['reports_yesterday'] = 'ໃນມື້ວານນີ້';
