8eb6b4a0dc33d8b1efcfe01a6fd748a1d380cfe0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:50:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'รายงานการตรวจสอบฐานให้ชุดของเครื่องมือรายงานมาตรฐานสำหรับระบบปฏิบัติการ';
+$lang['reports_app_name'] = 'รายงานฐาน';
+$lang['reports_date_range'] = 'ช่วงวันที่';
+$lang['reports_device_type_summary'] = 'ประเภทอุปกรณ์อย่างย่อ';
+$lang['reports_device_types'] = 'อุปกรณ์ประเภท';
+$lang['reports_entries'] = 'รายการ';
+$lang['reports_full_report'] = 'รายงานฉบับเต็ม';
+$lang['reports_ip_summary'] = 'IP ข้อมูลอย่างย่อ';
+$lang['reports_last_30_days'] = '30 วันล่าสุด';
+$lang['reports_last_7_days'] = '7 วันล่าสุด';
+$lang['reports_live'] = 'มีชีวิต';
+$lang['reports_range_invalid'] = 'ช่วงไม่ถูกต้อง';
+$lang['reports_report_data'] = 'ข้อมูลรายงาน';
+$lang['reports_report_settings'] = 'การตั้งค่ารายงาน';
+$lang['reports_reports'] = 'รายงาน';
+$lang['reports_today'] = 'ในวันนี้';
+$lang['reports_top_device_types'] = 'อุปกรณ์ประเภทสูงสุด';
+$lang['reports_top_external_ips'] = 'IP ที่ภายนอกสูงสุด';
+$lang['reports_top_ips'] = 'IP ที่ด้านบน';
+$lang['reports_top_users'] = 'ผู้ใช้สูงสุด';
+$lang['reports_user_summary'] = 'สรุปผู้ใช้';
+$lang['reports_yesterday'] = 'เมื่อวาน';
