07f50765cdc4ea3d6b1ef25985ae7dc3792f52da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:49:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Aplikácia Základné Reports poskytuje sadu štandardných správy nástrojov pre operačný systém.';
+$lang['reports_app_name'] = 'Základňové Správy';
+$lang['reports_date_range'] = 'časové obdobie';
+$lang['reports_device_type_summary'] = 'Typ zariadenia Súhrn';
+$lang['reports_device_types'] = 'typy zariadení';
+$lang['reports_entries'] = 'Vstupy';
+$lang['reports_full_report'] = 'úplná správa';
+$lang['reports_ip_summary'] = 'IP Zhrnutie';
+$lang['reports_last_30_days'] = 'Posledných 30 dní';
+$lang['reports_last_7_days'] = 'Posledných 7 dní';
+$lang['reports_live'] = 'živé';
+$lang['reports_range_invalid'] = 'Rozsah je neplatný.';
+$lang['reports_report_data'] = 'správa dát';
+$lang['reports_report_settings'] = 'nastavenie správa';
+$lang['reports_reports'] = 'správy';
+$lang['reports_today'] = 'dnes';
+$lang['reports_top_device_types'] = 'Najlepšie typy zariadení';
+$lang['reports_top_external_ips'] = 'Top Externý IP';
+$lang['reports_top_ips'] = 'najlepší IP';
+$lang['reports_top_users'] = 'Top užívatelia';
+$lang['reports_user_summary'] = 'Zhrnutie Užívateľ';
+$lang['reports_yesterday'] = 'včera';
