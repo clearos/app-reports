643f0fecff630bfe325f07c94ab8b9367a9361fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:42:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Η εφαρμογή Εκθέσεις Base παρέχει ένα σύνολο τυποποιημένων εργαλείων έκθεσης για το λειτουργικό σύστημα.';
+$lang['reports_app_name'] = 'Αναφορές βάση';
+$lang['reports_date_range'] = 'Εύρος ημερομηνιών';
+$lang['reports_device_type_summary'] = 'Τύπος συσκευής Περίληψη';
+$lang['reports_device_types'] = 'Τύποι συσκευής';
+$lang['reports_entries'] = 'καταχωρήσεις';
+$lang['reports_full_report'] = 'Πλήρης Αναφορά';
+$lang['reports_ip_summary'] = 'Περίληψη IP';
+$lang['reports_last_30_days'] = 'Τελευταίες 30 Ημέρες';
+$lang['reports_last_7_days'] = 'Τελευταίες 7 Ημέρες';
+$lang['reports_live'] = 'Ζω';
+$lang['reports_range_invalid'] = 'Εύρος δεν είναι έγκυρο.';
+$lang['reports_report_data'] = 'δεδομένα έκθεσης';
+$lang['reports_report_settings'] = 'Ρυθμίσεις έκθεσης';
+$lang['reports_reports'] = 'Αναφορές';
+$lang['reports_today'] = 'Σήμερα';
+$lang['reports_top_device_types'] = 'Top τύπους συσκευών';
+$lang['reports_top_external_ips'] = 'Κορυφαία εξωτερικές διευθύνσεις IP';
+$lang['reports_top_ips'] = 'Top IPs';
+$lang['reports_top_users'] = 'Top χρήστες';
+$lang['reports_user_summary'] = 'Περίληψη χρήστη';
+$lang['reports_yesterday'] = 'Εχθές';
