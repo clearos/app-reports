bc5662eb6cf531476f9f2fd2885a022232c780fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:47:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'De Base Reports app biedt een set van standaard rapport hulpmiddelen voor het besturingssysteem.';
+$lang['reports_app_name'] = 'Base Reports';
+$lang['reports_date_range'] = 'Datumbereik';
+$lang['reports_device_type_summary'] = 'Type apparaat Samenvatting';
+$lang['reports_device_types'] = 'Device Types';
+$lang['reports_entries'] = 'inzendingen';
+$lang['reports_full_report'] = 'Volledig rapport';
+$lang['reports_ip_summary'] = 'IP Samenvatting';
+$lang['reports_last_30_days'] = 'Laatste 30 dagen';
+$lang['reports_last_7_days'] = 'Laatste 7 dagen';
+$lang['reports_live'] = 'Leven';
+$lang['reports_range_invalid'] = 'Range is ongeldig.';
+$lang['reports_report_data'] = 'Rapportgegevens';
+$lang['reports_report_settings'] = 'Report Settings';
+$lang['reports_reports'] = 'rapporten';
+$lang['reports_today'] = 'Vandaag';
+$lang['reports_top_device_types'] = 'Top Device Types';
+$lang['reports_top_external_ips'] = 'Top Externe IPs';
+$lang['reports_top_ips'] = 'Top IPs';
+$lang['reports_top_users'] = 'Top Gebruikers';
+$lang['reports_user_summary'] = 'gebruiker Samenvatting';
+$lang['reports_yesterday'] = 'Gisteren';
