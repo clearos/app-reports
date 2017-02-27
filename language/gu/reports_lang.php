37929e04c685412109c9a8e05f461069f9f82466
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:42:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'બેઝ અહેવાલો એપ્લિકેશન ઓપરેટિંગ સિસ્ટમ માટે પ્રમાણભૂત અહેવાલ સાધનો સમૂહ પૂરો પાડે છે.';
+$lang['reports_app_name'] = 'બેઝ અહેવાલો';
+$lang['reports_date_range'] = 'તારીખ રેંજ';
+$lang['reports_device_type_summary'] = 'ઉપકરણ પ્રકાર સારાંશ';
+$lang['reports_device_types'] = 'ઉપકરણ પ્રકારો';
+$lang['reports_entries'] = 'પ્રવેશ';
+$lang['reports_full_report'] = 'સંપૂર્ણ અહેવાલ';
+$lang['reports_ip_summary'] = 'IP સારાંશ';
+$lang['reports_last_30_days'] = 'છેલ્લા 30 દિવસમાં';
+$lang['reports_last_7_days'] = 'છેલ્લા 7 દિવસો';
+$lang['reports_live'] = 'રહેવા';
+$lang['reports_range_invalid'] = 'રેંજ અમાન્ય છે.';
+$lang['reports_report_data'] = 'રિપોર્ટ ડેટા';
+$lang['reports_report_settings'] = 'રિપોર્ટ સેટિંગ્સ';
+$lang['reports_reports'] = 'અહેવાલ';
+$lang['reports_today'] = 'આજે';
+$lang['reports_top_device_types'] = 'ટોચના ઉપકરણ પ્રકારો';
+$lang['reports_top_external_ips'] = 'ટોચના બાહ્ય થયેલા IP';
+$lang['reports_top_ips'] = 'ટોચના થયેલા IP';
+$lang['reports_top_users'] = 'ટોચના વપરાશકર્તાઓ';
+$lang['reports_user_summary'] = 'વપરાશકર્તા સારાંશ';
+$lang['reports_yesterday'] = 'ગઇકાલે';
