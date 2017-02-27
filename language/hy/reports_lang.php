fdb63ebfcfff5aa8772af19f3b8053aa734eac0b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:39:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Բազայի Ռեպորտաժ ծրագիրը ապահովում է մի շարք ստանդարտ զեկույցի գործիքներ օպերացիոն համակարգի.';
+$lang['reports_app_name'] = 'բազային Ռեպորտաժ';
+$lang['reports_date_range'] = 'Ամսաթիվ Range';
+$lang['reports_device_type_summary'] = 'Device Type Ամփոփում';
+$lang['reports_device_types'] = 'սարքի տեսակները';
+$lang['reports_entries'] = 'Գրառումներ';
+$lang['reports_full_report'] = 'Full Report';
+$lang['reports_ip_summary'] = 'IP Ամփոփում';
+$lang['reports_last_30_days'] = 'Վերջին 30 Days';
+$lang['reports_last_7_days'] = 'Վերջին 7 Days';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range է անվավեր:';
+$lang['reports_report_data'] = 'Report Data';
+$lang['reports_report_settings'] = 'Հաղորդել Կառավարում';
+$lang['reports_reports'] = 'Զեկույցներ';
+$lang['reports_today'] = 'այսօր';
+$lang['reports_top_device_types'] = 'Ամենաշատ Սարքի տեսակները';
+$lang['reports_top_external_ips'] = 'Top Արտաքին IP- ներ';
+$lang['reports_top_ips'] = 'Ամենաշատ IP- ներ';
+$lang['reports_top_users'] = 'Ամենաշատ Users';
+$lang['reports_user_summary'] = 'Օգտվող Ամփոփում';
+$lang['reports_yesterday'] = 'երեկ';
