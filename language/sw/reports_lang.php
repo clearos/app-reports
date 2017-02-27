117bfcc94207aad79fe6a2b890a977fecc8aab85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:50:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'programu Ripoti Msingi inatoa seti ya zana kiwango ripoti kwa mfumo wa uendeshaji.';
+$lang['reports_app_name'] = 'Ripoti msingi';
+$lang['reports_date_range'] = 'tarehe Mbalimbali';
+$lang['reports_device_type_summary'] = 'Aina kifaa Summary';
+$lang['reports_device_types'] = 'kifaa Aina';
+$lang['reports_entries'] = 'entries';
+$lang['reports_full_report'] = 'Ripoti kamili';
+$lang['reports_ip_summary'] = 'IP Summary';
+$lang['reports_last_30_days'] = 'Siku za Mwisho 30';
+$lang['reports_last_7_days'] = 'Mwisho 7 Days';
+$lang['reports_live'] = 'kuishi';
+$lang['reports_range_invalid'] = 'Mbalimbali ni batili.';
+$lang['reports_report_data'] = 'ripoti Data';
+$lang['reports_report_settings'] = 'ripoti Settings';
+$lang['reports_reports'] = 'ripoti';
+$lang['reports_today'] = 'leo';
+$lang['reports_top_device_types'] = 'Juu hila Aina';
+$lang['reports_top_external_ips'] = 'Juu External IPS';
+$lang['reports_top_ips'] = 'juu IPS';
+$lang['reports_top_users'] = 'Watumiaji wakuu';
+$lang['reports_user_summary'] = 'Summary mtumiaji';
+$lang['reports_yesterday'] = 'jana';
