d2f70e750b9a86c998a346ce97b7b46ff1e95802
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:41:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'La Bazo Raportoj app provizas aro de normo raporto iloj por la mastruma sistemo.';
+$lang['reports_app_name'] = 'bazo Raportoj';
+$lang['reports_date_range'] = 'Dato Gamo';
+$lang['reports_device_type_summary'] = 'Aparato Tipo Resumo';
+$lang['reports_device_types'] = 'aparato Tipoj';
+$lang['reports_entries'] = 'Aktualaĵoj';
+$lang['reports_full_report'] = 'plena Raporto';
+$lang['reports_ip_summary'] = 'IP Resumo';
+$lang['reports_last_30_days'] = 'Lasta 30 Tagoj';
+$lang['reports_last_7_days'] = 'Lasta 7 Tagoj';
+$lang['reports_live'] = 'rekta';
+$lang['reports_range_invalid'] = 'Gamo estas nevalida.';
+$lang['reports_report_data'] = 'Raporti Datumoj';
+$lang['reports_report_settings'] = 'Raporti Agordoj';
+$lang['reports_reports'] = 'raportoj';
+$lang['reports_today'] = 'hodiaŭ';
+$lang['reports_top_device_types'] = 'Supro Aparato Tipoj';
+$lang['reports_top_external_ips'] = 'Supro Eksteraj IPs';
+$lang['reports_top_ips'] = 'supro IPs';
+$lang['reports_top_users'] = 'supro Uzantoj';
+$lang['reports_user_summary'] = 'uzanto Resumo';
+$lang['reports_yesterday'] = 'hieraŭ';
