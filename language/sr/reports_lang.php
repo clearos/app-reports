277c16b0d5fe8888091d28449f40125bbc9bc71c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:48:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Тхе Басе Извештаји апликација пружа скуп стандардних извештаја алата за оперативни систем.';
+$lang['reports_app_name'] = 'Басе Извештаји';
+$lang['reports_date_range'] = 'Датум Опсег';
+$lang['reports_device_type_summary'] = 'Тип уређаја Преглед';
+$lang['reports_device_types'] = 'Типови уређаја';
+$lang['reports_entries'] = 'уноса';
+$lang['reports_full_report'] = 'Детаљни извештај';
+$lang['reports_ip_summary'] = 'ИП-Преглед';
+$lang['reports_last_30_days'] = 'Последњих 30 дана';
+$lang['reports_last_7_days'] = 'Последњих 7 дана';
+$lang['reports_live'] = 'ливе';
+$lang['reports_range_invalid'] = 'Опсег је неважећи.';
+$lang['reports_report_data'] = 'Дата репорт';
+$lang['reports_report_settings'] = 'Репорт Подешавања';
+$lang['reports_reports'] = 'Извештаји';
+$lang['reports_today'] = 'Данас';
+$lang['reports_top_device_types'] = 'Топ Типови уређаја';
+$lang['reports_top_external_ips'] = 'Топ Спољни ИП адреса';
+$lang['reports_top_ips'] = 'Топ ИП адреса';
+$lang['reports_top_users'] = 'Топ';
+$lang['reports_user_summary'] = 'Корисник Преглед';
+$lang['reports_yesterday'] = 'јуче';
