9bcfe2238dbb1caab536422c5d89665eaa2788a3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:44:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'របាយការណ៍មូលដ្ឋានកម្មវិធីផ្ដល់នូវសំណុំនៃឧបករណ៍របាយការណ៍ស្តង់ដារសម្រាប់ប្រព័ន្ធប្រតិបត្តិការ។';
+$lang['reports_app_name'] = 'របាយការណ៍មូលដ្ឋាន';
+$lang['reports_date_range'] = 'ជួរកាលបរិច្ឆេទ';
+$lang['reports_device_type_summary'] = 'ប្រភេទឧបករណ៍សង្ខេប';
+$lang['reports_device_types'] = 'ប្រភេទឧបករណ៍';
+$lang['reports_entries'] = 'ធាតុ';
+$lang['reports_full_report'] = 'របាយការណ៍ពេញ';
+$lang['reports_ip_summary'] = 'សង្ខេប IP របស់';
+$lang['reports_last_30_days'] = '30 ថ្ងៃចុងក្រោយ';
+$lang['reports_last_7_days'] = '7 ថ្ងៃចុងក្រោយ';
+$lang['reports_live'] = 'ការផ្សាយបន្តផ្ទាល់';
+$lang['reports_range_invalid'] = 'ជួរមិនត្រឹមត្រូវ។';
+$lang['reports_report_data'] = 'ទិន្នន័យរបាយការណ៍';
+$lang['reports_report_settings'] = 'ការកំណត់របាយការណ៍';
+$lang['reports_reports'] = 'របាយការណ៍';
+$lang['reports_today'] = 'ថ្ងៃនេះ';
+$lang['reports_top_device_types'] = 'ប្រភេទឧបករណ៍កំពូល';
+$lang['reports_top_external_ips'] = 'កំពូលជនជាតិដើមភាគតិចខាងក្រៅ';
+$lang['reports_top_ips'] = 'ជនជាតិដើមភាគតិចកំពូល';
+$lang['reports_top_users'] = 'អ្នកប្រើកំពូល';
+$lang['reports_user_summary'] = 'សេចក្តីសង្ខេបរបស់អ្នកប្រើ';
+$lang['reports_yesterday'] = 'កាលពីម្សិលមិញ';
