1ac8ab63701038102312b440e99f4a81ee2c684f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:48:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Aplicația Rapoarte de bază oferă un set de instrumente de raportare standard pentru sistemul de operare.';
+$lang['reports_app_name'] = 'Rapoarte de bază';
+$lang['reports_date_range'] = 'Interval de date';
+$lang['reports_device_type_summary'] = 'Dispozitiv Tip Rezumat';
+$lang['reports_device_types'] = 'Tipuri de dispozitive';
+$lang['reports_entries'] = 'intrările';
+$lang['reports_full_report'] = 'Raport întreg';
+$lang['reports_ip_summary'] = 'Sumar IP';
+$lang['reports_last_30_days'] = 'Ultimele 30 de zile';
+$lang['reports_last_7_days'] = 'Ultimele 7 zile';
+$lang['reports_live'] = 'Trăi';
+$lang['reports_range_invalid'] = 'Gama este nevalid.';
+$lang['reports_report_data'] = 'raport de date';
+$lang['reports_report_settings'] = 'Setări de rapoarte';
+$lang['reports_reports'] = 'rapoarte';
+$lang['reports_today'] = 'Astăzi';
+$lang['reports_top_device_types'] = 'Tipuri de dispozitive de top';
+$lang['reports_top_external_ips'] = 'Top externe IP-uri';
+$lang['reports_top_ips'] = 'Top IP-uri';
+$lang['reports_top_users'] = 'Top utilizatori';
+$lang['reports_user_summary'] = 'Sumar utilizator';
+$lang['reports_yesterday'] = 'Ieri';
