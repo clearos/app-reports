f21ac22b7aea3562c584f44ad3d0819ae1cfcee9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:51:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Ang Base Ulat app ay nagbibigay ng isang hanay ng mga standard na mga kasangkapan ulat para sa mga operating system.';
+$lang['reports_app_name'] = 'base Reports';
+$lang['reports_date_range'] = 'Hanay ng Petsa';
+$lang['reports_device_type_summary'] = 'Uri ng Device Buod';
+$lang['reports_device_types'] = 'Uri aparato';
+$lang['reports_entries'] = 'entries';
+$lang['reports_full_report'] = 'Buong Report';
+$lang['reports_ip_summary'] = 'IP Buod';
+$lang['reports_last_30_days'] = 'Huling 30 Araw';
+$lang['reports_last_7_days'] = 'Huling 7 Araw';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Saklaw ay hindi wasto.';
+$lang['reports_report_data'] = 'Isumbong ang Data';
+$lang['reports_report_settings'] = 'Setting Report';
+$lang['reports_reports'] = 'Reports';
+$lang['reports_today'] = 'ngayon';
+$lang['reports_top_device_types'] = 'Top Uri aparato';
+$lang['reports_top_external_ips'] = 'Top Panlabas IPs';
+$lang['reports_top_ips'] = 'Top IPs';
+$lang['reports_top_users'] = 'Mga Nangungunang Tagagamit';
+$lang['reports_user_summary'] = 'Buod User';
+$lang['reports_yesterday'] = 'kahapon';
