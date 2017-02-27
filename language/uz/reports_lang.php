2cab4f8b64b8a855f2c83cd4d1ffb632111a821b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:51:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Base hisobotlar ilova operatsion tizimi uchun standart hisobot vositalari majmuini beradi.';
+$lang['reports_app_name'] = 'Base hisobotlar';
+$lang['reports_date_range'] = 'Sana oralig&#39;i';
+$lang['reports_device_type_summary'] = 'Device turi Xulosa';
+$lang['reports_device_types'] = 'Device turlari';
+$lang['reports_entries'] = 'yozuvlari';
+$lang['reports_full_report'] = 'to&#39;liq hisobot';
+$lang['reports_ip_summary'] = 'IP Xulosa';
+$lang['reports_last_30_days'] = 'Oxirgi 30 kun';
+$lang['reports_last_7_days'] = 'Oxirgi 7 Days';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range haqiqiy emas.';
+$lang['reports_report_data'] = 'hisobot Data';
+$lang['reports_report_settings'] = 'hisobot Sozlamalar';
+$lang['reports_reports'] = 'Hisobotlar';
+$lang['reports_today'] = 'Bugun';
+$lang['reports_top_device_types'] = 'Top Device turlari';
+$lang['reports_top_external_ips'] = 'Top Tashqi ilmim';
+$lang['reports_top_ips'] = 'Top-ilmim';
+$lang['reports_top_users'] = 'Top foydalanuvchilar';
+$lang['reports_user_summary'] = 'foydalanuvchi Xulosa';
+$lang['reports_yesterday'] = 'kecha';
