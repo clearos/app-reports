0e7df9a150030cb428038ec3562ec4ff1b72392d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:43:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Laporan Basis aplikasi menyediakan satu set alat laporan standar untuk sistem operasi.';
+$lang['reports_app_name'] = 'Laporan basis';
+$lang['reports_date_range'] = 'Rentang Tanggal';
+$lang['reports_device_type_summary'] = 'Perangkat Type Summary';
+$lang['reports_device_types'] = 'Jenis perangkat';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Laporan Lengkap';
+$lang['reports_ip_summary'] = 'IP Ringkasan';
+$lang['reports_last_30_days'] = '30 Hari Terakhir';
+$lang['reports_last_7_days'] = '7 Hari Terakhir';
+$lang['reports_live'] = 'Hidup';
+$lang['reports_range_invalid'] = 'Rentang tidak valid.';
+$lang['reports_report_data'] = 'data laporan';
+$lang['reports_report_settings'] = 'Pengaturan laporan';
+$lang['reports_reports'] = 'laporan';
+$lang['reports_today'] = 'Hari ini';
+$lang['reports_top_device_types'] = 'Jenis Perangkat Top';
+$lang['reports_top_external_ips'] = 'Top IP Eksternal';
+$lang['reports_top_ips'] = 'IP atas';
+$lang['reports_top_users'] = 'Pengguna Top';
+$lang['reports_user_summary'] = 'Ringkasan pengguna';
+$lang['reports_yesterday'] = 'Kemarin';
