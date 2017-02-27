a2746201c52d6cfd6107b7f4886b39d537817dce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:40:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'အဆိုပါတပ်စခန်း app ကို operating system အတွက်စံအစီရင်ခံစာ tools တွေကိုအစုတခုပေးစွမ်းအစီရင်ခံစာများ။';
+$lang['reports_app_name'] = 'base အစီရင်ခံစာများ';
+$lang['reports_date_range'] = 'နေ့စွဲ Range';
+$lang['reports_device_type_summary'] = 'device အမျိုးအစားအကျဉ်းချုပ်';
+$lang['reports_device_types'] = 'စက်ပစ္စည်းအမျိုးအစားများ';
+$lang['reports_entries'] = 'entries';
+$lang['reports_full_report'] = 'အပြည့်အဝအစီရင်ခံစာ';
+$lang['reports_ip_summary'] = 'အိုင်ပီအကျဉ်းချုပ်';
+$lang['reports_last_30_days'] = 'ပြီးခဲ့သည့် 30 ရက်နေ့ရက်များ';
+$lang['reports_last_7_days'] = 'နောက်ဆုံး 7 နေ့ရက်များ';
+$lang['reports_live'] = 'အသက်ရှင်သော';
+$lang['reports_range_invalid'] = 'အကွာအဝေးမမှန်ကန်ဘူး။';
+$lang['reports_report_data'] = 'အစီရင်ခံစာ Data ကို';
+$lang['reports_report_settings'] = 'အစီရင်ခံစာက Settings';
+$lang['reports_reports'] = 'အစီရင်ခံစာများ';
+$lang['reports_today'] = 'ယနေ့တွင်';
+$lang['reports_top_device_types'] = 'ထိပ်တန်းစက်ပစ္စည်းအမျိုးအစားများ';
+$lang['reports_top_external_ips'] = 'ထိပ်တန်းပြင်ပမိတ်ဖက်';
+$lang['reports_top_ips'] = 'ထိပ်တန်းမိတ်ဖက်';
+$lang['reports_top_users'] = 'ထိပ်တန်းအသုံးပြုသူများ';
+$lang['reports_user_summary'] = 'အသုံးပြုသူအကျဉ်းချုပ်';
+$lang['reports_yesterday'] = 'မနေ့က';
