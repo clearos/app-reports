89ffcf4dcbf7f3314bd5ece7b04a5027d9ba15c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:48:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'යෙදුම මෙහෙයුම් පද්ධතිය සඳහා සම්මත වාර්තාව මෙවලම් කට්ටලයක් ලබා දෙන මූලික වාර්තා.';
+$lang['reports_app_name'] = 'මූලික වාර්තා';
+$lang['reports_date_range'] = 'දිනය රංගේ';
+$lang['reports_device_type_summary'] = 'උපාංගය වර්ගය සාරාංශය';
+$lang['reports_device_types'] = 'උපාංගය වර්ග';
+$lang['reports_entries'] = 'සටහන් ඇතුළත් කිරීම්';
+$lang['reports_full_report'] = 'පූර්ණ වාර්තාව';
+$lang['reports_ip_summary'] = 'IP සාරාංශය';
+$lang['reports_last_30_days'] = 'පසුගිය දින 30';
+$lang['reports_last_7_days'] = 'අන්තිම දවස් 7';
+$lang['reports_live'] = 'සජීවි';
+$lang['reports_range_invalid'] = 'රංගේ වලංගු නැත.';
+$lang['reports_report_data'] = 'වාර්තාව දත්ත';
+$lang['reports_report_settings'] = 'වාර්තාව සැකසීම්';
+$lang['reports_reports'] = 'වාර්තා';
+$lang['reports_today'] = 'අද';
+$lang['reports_top_device_types'] = 'Top උපාංගය වර්ග';
+$lang['reports_top_external_ips'] = 'Top විදේශ ස්ථානාධිපතිවරයාට';
+$lang['reports_top_ips'] = 'Top ස්ථානාධිපතිවරයාට';
+$lang['reports_top_users'] = 'Top පරිශීලකයන්';
+$lang['reports_user_summary'] = 'පරිශීලක සාරාංශය';
+$lang['reports_yesterday'] = 'ඊයේ';
