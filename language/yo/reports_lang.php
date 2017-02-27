9ae29c8cdc93bee81c63b5a1614e85e91502780e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:52:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Awọn Mimọ Iroyin app pese a ti ṣeto ti boṣewa Iroyin irinṣẹ fun awọn ẹrọ eto.';
+$lang['reports_app_name'] = 'mimọ Iroyin';
+$lang['reports_date_range'] = 'ọjọ Range';
+$lang['reports_device_type_summary'] = 'Device Type Lakotan';
+$lang['reports_device_types'] = 'Device Orisi';
+$lang['reports_entries'] = 'awọn titẹ sii';
+$lang['reports_full_report'] = 'Full Iroyin';
+$lang['reports_ip_summary'] = 'IP Lakotan';
+$lang['reports_last_30_days'] = '30 Ọjọ';
+$lang['reports_last_7_days'] = 'Kẹhin 7 Ọjọ';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Ibiti o ti wa ni invalid.';
+$lang['reports_report_data'] = 'Iroyin Data';
+$lang['reports_report_settings'] = 'Iroyin Eto';
+$lang['reports_reports'] = 'iroyin';
+$lang['reports_today'] = 'loni';
+$lang['reports_top_device_types'] = 'Top Device Orisi';
+$lang['reports_top_external_ips'] = 'Top Ita IPs';
+$lang['reports_top_ips'] = 'Top IPs';
+$lang['reports_top_users'] = 'Top oníṣe';
+$lang['reports_user_summary'] = 'User Lakotan';
+$lang['reports_yesterday'] = 'lana';
