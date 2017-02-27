6b9532cac8221e13edf84b37952c3e0a3ca84382
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:47:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Il-app Rapporti Bażi jipprovdi sett ta &#39;għodod rapport standard għas-sistema operattiva.';
+$lang['reports_app_name'] = 'Rapporti Bażi';
+$lang['reports_date_range'] = 'data Medda';
+$lang['reports_device_type_summary'] = 'Apparat Tip Sommarju';
+$lang['reports_device_types'] = 'Tipi apparat';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Rapport sħiħ';
+$lang['reports_ip_summary'] = 'Sommarju PI';
+$lang['reports_last_30_days'] = 'Last 30 Days';
+$lang['reports_last_7_days'] = 'Aħħar 7 ijiem';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Medda huwa invalidu.';
+$lang['reports_report_data'] = 'rapport Dejta';
+$lang['reports_report_settings'] = 'rapport Settings';
+$lang['reports_reports'] = 'rapporti';
+$lang['reports_today'] = 'Illum';
+$lang['reports_top_device_types'] = 'Tipi Apparat Top';
+$lang['reports_top_external_ips'] = 'Top IPs Esterni';
+$lang['reports_top_ips'] = 'IPs top';
+$lang['reports_top_users'] = 'Utenti top';
+$lang['reports_user_summary'] = 'utent Sommarju';
+$lang['reports_yesterday'] = 'Ilbieraħ';
