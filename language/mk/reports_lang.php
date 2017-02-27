05736ac367382e4e29872b7c2ea0b1c1ffcd9515
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:46:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'стан на база извештаи предвидува сет на стандардни алатки извештај за оперативниот систем.';
+$lang['reports_app_name'] = 'база Извештаи';
+$lang['reports_date_range'] = 'Период';
+$lang['reports_device_type_summary'] = 'Тип на уред Резиме';
+$lang['reports_device_types'] = 'Уредите од типот';
+$lang['reports_entries'] = 'записи';
+$lang['reports_full_report'] = 'Комплетен извештај';
+$lang['reports_ip_summary'] = 'IP Резиме';
+$lang['reports_last_30_days'] = 'Последните 30 дена';
+$lang['reports_last_7_days'] = 'Последните 7 дена';
+$lang['reports_live'] = 'Во живо';
+$lang['reports_range_invalid'] = 'Опсег не е валиден.';
+$lang['reports_report_data'] = 'Пријави податоци';
+$lang['reports_report_settings'] = 'Пријави Settings';
+$lang['reports_reports'] = 'извештаи';
+$lang['reports_today'] = 'денес';
+$lang['reports_top_device_types'] = 'Топ Уредите од типот';
+$lang['reports_top_external_ips'] = 'Топ надворешни IP адреси';
+$lang['reports_top_ips'] = 'Топ IP адреси';
+$lang['reports_top_users'] = 'Членови кои го';
+$lang['reports_user_summary'] = 'корисникот Резиме';
+$lang['reports_yesterday'] = 'вчера';
