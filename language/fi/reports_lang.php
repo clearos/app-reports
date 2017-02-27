47cb60120a30efd5e2d78bac277b422045947402
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:42:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Base Reports sovellus tarjoaa joukon vakioraportin työkaluja käyttöjärjestelmän.';
+$lang['reports_app_name'] = 'Base Raportit';
+$lang['reports_date_range'] = 'Ajanjakso';
+$lang['reports_device_type_summary'] = 'Laitteen tyyppi Yhteenveto';
+$lang['reports_device_types'] = 'Laiteetyypit';
+$lang['reports_entries'] = 'merkinnät';
+$lang['reports_full_report'] = 'Koko raportti';
+$lang['reports_ip_summary'] = 'IP Yhteenveto';
+$lang['reports_last_30_days'] = 'Viimeiset 30 päivää';
+$lang['reports_last_7_days'] = 'Viimeiset 7 päivää';
+$lang['reports_live'] = 'Elää';
+$lang['reports_range_invalid'] = 'Range on virheellinen.';
+$lang['reports_report_data'] = 'raporttitietojen';
+$lang['reports_report_settings'] = 'Raportin asetukset';
+$lang['reports_reports'] = 'raportit';
+$lang['reports_today'] = 'Tänään';
+$lang['reports_top_device_types'] = 'Top Laiteetyypit';
+$lang['reports_top_external_ips'] = 'Top Ulkoiset IP';
+$lang['reports_top_ips'] = 'Top IP';
+$lang['reports_top_users'] = 'Suosituimmat käyttäjät';
+$lang['reports_user_summary'] = 'Käyttäjä Yhteenveto';
+$lang['reports_yesterday'] = 'Eilen';
