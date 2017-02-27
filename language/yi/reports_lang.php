2a87f58223b5ec5bb69a791096ccbf2b875f513b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:52:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'די באַזע רעפּאָרץ אַפּ גיט אַ סכום פון נאָרמאַל באַריכט מכשירים פֿאַר די אָפּערייטינג סיסטעם.';
+$lang['reports_app_name'] = 'באַזע רעפּאָרץ';
+$lang['reports_date_range'] = 'דאַטע קייט';
+$lang['reports_device_type_summary'] = 'מיטל טיפּ סאַמערי';
+$lang['reports_device_types'] = 'מיטל טייפּס';
+$lang['reports_entries'] = 'איינסן';
+$lang['reports_full_report'] = 'גאַנץ באריכט';
+$lang['reports_ip_summary'] = 'יפּ סאַמערי';
+$lang['reports_last_30_days'] = 'לעצטע 30 טעג';
+$lang['reports_last_7_days'] = 'לעצטע 7 טעג';
+$lang['reports_live'] = 'לעבן';
+$lang['reports_range_invalid'] = 'ראַנגע איז פאַרקריפּלט.';
+$lang['reports_report_data'] = 'באריכט דאַטאַ';
+$lang['reports_report_settings'] = 'באריכט סעטטינגס';
+$lang['reports_reports'] = 'רעפּאָרץ';
+$lang['reports_today'] = 'הייַנט';
+$lang['reports_top_device_types'] = 'שפּיץ דיווייס טייפּס';
+$lang['reports_top_external_ips'] = 'שפּיץ עקסטערנאַל יפּס';
+$lang['reports_top_ips'] = 'שפּיץ יפּס';
+$lang['reports_top_users'] = 'שפּיץ ניצערס';
+$lang['reports_user_summary'] = 'באַניצער סאַמערי';
+$lang['reports_yesterday'] = 'נעכטן';
