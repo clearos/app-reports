fbfdef5a4abc9c5248480dcc90e3ea7e67477cce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:46:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Үндсэн тайлан апп үйлдлийн системийн стандарт тайлан хэрэгслүүдийн цогц олгодог.';
+$lang['reports_app_name'] = 'үндсэн тайлан';
+$lang['reports_date_range'] = 'огноо Range';
+$lang['reports_device_type_summary'] = 'Төхөөрөмжийн төрөл тойм';
+$lang['reports_device_types'] = 'төхөөрөмжийн төрөл';
+$lang['reports_entries'] = 'оруулгууд';
+$lang['reports_full_report'] = 'бүрэн тайлан';
+$lang['reports_ip_summary'] = 'IP тойм';
+$lang['reports_last_30_days'] = 'Сүүлийн 30 хоног';
+$lang['reports_last_7_days'] = 'Сүүлийн 7 хоног';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range хүчин төгөлдөр бус байна.';
+$lang['reports_report_data'] = 'тайлан мэдээлэл';
+$lang['reports_report_settings'] = 'тайлан Тохиргоо';
+$lang['reports_reports'] = 'тайлан';
+$lang['reports_today'] = 'Өнөөдөр';
+$lang['reports_top_device_types'] = 'Топ Төхөөрөмжийн төрөл';
+$lang['reports_top_external_ips'] = 'Топ гадаад IP нь';
+$lang['reports_top_ips'] = 'топ IP нь';
+$lang['reports_top_users'] = 'топ гишүүд';
+$lang['reports_user_summary'] = 'Хэрэглэгчийн тойм';
+$lang['reports_yesterday'] = 'Өчигдөр';
