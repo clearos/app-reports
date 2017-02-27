0e20d7a89114d532a188ebce73444f14e2567394
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:46:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'बेस अहवाल अनुप्रयोग ऑपरेटिंग सिस्टम मानक अहवाल साधने पुरवते.';
+$lang['reports_app_name'] = 'बेस अहवाल';
+$lang['reports_date_range'] = 'तारीख श्रेणी';
+$lang['reports_device_type_summary'] = 'सारांश डिव्हाइस प्रकार';
+$lang['reports_device_types'] = 'साधन कार';
+$lang['reports_entries'] = 'नोंदी';
+$lang['reports_full_report'] = 'पूर्ण अहवाल';
+$lang['reports_ip_summary'] = 'IP सारांश';
+$lang['reports_last_30_days'] = 'अंतिम 30 दिवस';
+$lang['reports_last_7_days'] = 'अंतिम 7 दिवस';
+$lang['reports_live'] = 'राहतात';
+$lang['reports_range_invalid'] = 'श्रेणी अवैध आहे.';
+$lang['reports_report_data'] = 'डेटा अहवाल';
+$lang['reports_report_settings'] = 'अहवाल सेटिंग्ज';
+$lang['reports_reports'] = 'अहवाल';
+$lang['reports_today'] = 'आज';
+$lang['reports_top_device_types'] = 'शीर्ष साधन कार';
+$lang['reports_top_external_ips'] = 'शीर्ष बाह्य दिलेले IP';
+$lang['reports_top_ips'] = 'शीर्ष दिलेले IP';
+$lang['reports_top_users'] = 'प्रमुख वापरकर्ते';
+$lang['reports_user_summary'] = 'वापरकर्ता सारांश';
+$lang['reports_yesterday'] = 'काल';
