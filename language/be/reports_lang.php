0a217e744f98106f01596b8ff9cf269a495a03af
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:40:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Дадатак базавых справаздач падае набор стандартных інструментаў пабудовы справаздач для аперацыйнай сістэмы.';
+$lang['reports_app_name'] = 'базавыя справаздачы';
+$lang['reports_date_range'] = 'дыяпазон дат';
+$lang['reports_device_type_summary'] = 'Тып прылады Дадатковая інфармацыя';
+$lang['reports_device_types'] = 'тыпы прылад';
+$lang['reports_entries'] = 'запісы';
+$lang['reports_full_report'] = 'поўны справаздачу';
+$lang['reports_ip_summary'] = 'IP Дадатковая інфармацыя';
+$lang['reports_last_30_days'] = 'Апошнія 30 дзён';
+$lang['reports_last_7_days'] = 'За апошнія 7 дзён';
+$lang['reports_live'] = 'жывая';
+$lang['reports_range_invalid'] = 'Дыяпазон з&#39;яўляецца несапраўдным.';
+$lang['reports_report_data'] = 'дадзеныя справаздачы';
+$lang['reports_report_settings'] = 'налады справаздачы';
+$lang['reports_reports'] = 'справаздачы';
+$lang['reports_today'] = 'сёння';
+$lang['reports_top_device_types'] = 'Лепшыя тыпы прылад';
+$lang['reports_top_external_ips'] = 'Топ Знешнія IP-адрасы';
+$lang['reports_top_ips'] = 'Лепшыя IP-адрасы';
+$lang['reports_top_users'] = 'топ карыстальнікаў';
+$lang['reports_user_summary'] = 'рэзюмэ карыстальніка';
+$lang['reports_yesterday'] = 'учора';
