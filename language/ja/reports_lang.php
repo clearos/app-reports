b70c74b5b1febc4cf2ab1b4a7547a59e487e3041
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:44:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'ベースレポートアプリは、オペレーティング・システムのための標準的なレポートツールのセットを提供します。';
+$lang['reports_app_name'] = '基本レポート';
+$lang['reports_date_range'] = '日付範囲';
+$lang['reports_device_type_summary'] = 'デバイスタイプの概要';
+$lang['reports_device_types'] = 'デバイスタイプ';
+$lang['reports_entries'] = 'エントリー';
+$lang['reports_full_report'] = '詳細な報告書';
+$lang['reports_ip_summary'] = 'IPの概要';
+$lang['reports_last_30_days'] = '最後の30日間';
+$lang['reports_last_7_days'] = '最後の7日間';
+$lang['reports_live'] = 'ライブ';
+$lang['reports_range_invalid'] = '範囲は無効です。';
+$lang['reports_report_data'] = 'レポートデータ';
+$lang['reports_report_settings'] = 'レポート設定';
+$lang['reports_reports'] = 'レポート';
+$lang['reports_today'] = '今日';
+$lang['reports_top_device_types'] = 'トップデバイスタイプ';
+$lang['reports_top_external_ips'] = 'トップ外部IPアドレス';
+$lang['reports_top_ips'] = 'トップIPアドレス';
+$lang['reports_top_users'] = 'トップユーザー';
+$lang['reports_user_summary'] = 'ユーザーの概要';
+$lang['reports_yesterday'] = '昨日';
