170658f6e958bcbf6fdb058f03bc77265738a410
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:41:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Baas Reports app pakub kogum standard aruande vahendid operatsioonisüsteemi.';
+$lang['reports_app_name'] = 'Base aruanded';
+$lang['reports_date_range'] = 'Kuupäevavahemik';
+$lang['reports_device_type_summary'] = 'Seadme tüüp kokkuvõte';
+$lang['reports_device_types'] = 'seade liigid';
+$lang['reports_entries'] = 'Sissekanded';
+$lang['reports_full_report'] = 'Täielik raport';
+$lang['reports_ip_summary'] = 'IP kokkuvõte';
+$lang['reports_last_30_days'] = 'Viimase 30';
+$lang['reports_last_7_days'] = 'Viimased 7 päeva';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range on kehtetu.';
+$lang['reports_report_data'] = 'aruande andmeid';
+$lang['reports_report_settings'] = 'aruanne seaded';
+$lang['reports_reports'] = 'Teated';
+$lang['reports_today'] = 'täna';
+$lang['reports_top_device_types'] = 'Top Seadme liigid';
+$lang['reports_top_external_ips'] = 'Top Väline IP-aadressi';
+$lang['reports_top_ips'] = 'Top IP-aadressi';
+$lang['reports_top_users'] = 'Top Kasutajad';
+$lang['reports_user_summary'] = 'Kasutaja kokkuvõte';
+$lang['reports_yesterday'] = 'eile';
