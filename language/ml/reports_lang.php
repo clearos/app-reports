f62ccf7cdd51c770429df0b52ce812d7e5ff9a8c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:46:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'ബേസ് അപ്ലിക്കേഷൻ ഓപ്പറേറ്റിങ് സിസ്റ്റം സ്റ്റാൻഡേർഡ് റിപ്പോർട്ട് ഉപകരണങ്ങളുടെ ഒരു സെറ്റ് നൽകുന്നു റിപ്പോർട്ടുകൾ.';
+$lang['reports_app_name'] = 'ബേസ് റിപ്പോർട്ടുകൾ';
+$lang['reports_date_range'] = 'തീയതി ശ്രേണി';
+$lang['reports_device_type_summary'] = 'ഉപകരണ തരം ചുരുക്കം';
+$lang['reports_device_types'] = 'ഡിവൈസ് തരങ്ങളും';
+$lang['reports_entries'] = 'എൻട്രികൾ';
+$lang['reports_full_report'] = 'പൂർണ്ണ റിപ്പോർട്ട്';
+$lang['reports_ip_summary'] = 'ഐപി ചുരുക്കം';
+$lang['reports_last_30_days'] = 'കഴിഞ്ഞ 30 ദിവസങ്ങൾ';
+$lang['reports_last_7_days'] = 'കഴിഞ്ഞ 7 ദിവസം';
+$lang['reports_live'] = 'ലൈവ്';
+$lang['reports_range_invalid'] = 'റേഞ്ച് അസാധുവാണ്.';
+$lang['reports_report_data'] = 'റിപ്പോർട്ട് ഡാറ്റ';
+$lang['reports_report_settings'] = 'റിപ്പോർട്ട് ക്രമീകരണങ്ങൾ';
+$lang['reports_reports'] = 'റിപ്പോർട്ടുകൾ';
+$lang['reports_today'] = 'ഇന്ന്';
+$lang['reports_top_device_types'] = 'ടോപ്പ് ഡിവൈസ് തരങ്ങളും';
+$lang['reports_top_external_ips'] = 'മികച്ച ബാഹ്യ IP കൾ';
+$lang['reports_top_ips'] = 'ടോപ്പ് IP കൾ';
+$lang['reports_top_users'] = 'മികച്ച ഉപയോക്താക്കൾ';
+$lang['reports_user_summary'] = 'ഉപയോക്തൃ ചുരുക്കം';
+$lang['reports_yesterday'] = 'ഇന്നലെ';
