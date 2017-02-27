b34ef338eb5de07a58caf1ae8b09c0f94efadc54
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:40:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'The app Malipoti Base imapereka Zida zogwiritsira muyezo lipoti la opaleshoni dongosolo.';
+$lang['reports_app_name'] = 'Malipoti Base';
+$lang['reports_date_range'] = 'Date osiyanasiyana';
+$lang['reports_device_type_summary'] = 'Type chipangizo Chidule';
+$lang['reports_device_types'] = 'chipangizo Mitundu';
+$lang['reports_entries'] = 'zolemba';
+$lang['reports_full_report'] = 'Report Full';
+$lang['reports_ip_summary'] = 'IP Chidule';
+$lang['reports_last_30_days'] = 'Last Masiku 30';
+$lang['reports_last_7_days'] = 'Last 7 Masiku';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Osiyanasiyana ndi opanda pake.';
+$lang['reports_report_data'] = 'Report Data';
+$lang['reports_report_settings'] = 'Report Zikhazikiko';
+$lang['reports_reports'] = 'malipoti';
+$lang['reports_today'] = 'Today';
+$lang['reports_top_device_types'] = 'Top Chipangizo Mitundu';
+$lang['reports_top_external_ips'] = 'Top Zowonekera kunja IPS';
+$lang['reports_top_ips'] = 'Top IPS';
+$lang['reports_top_users'] = 'Top Ogwiritsa';
+$lang['reports_user_summary'] = 'Chidule wosuta';
+$lang['reports_yesterday'] = 'dzulo';
