4e762204ce594140475842f63039ef54fc79aa1c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:39:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'App Raportet Baza ofron një grup të mjeteve standarde të raportit për sistemin operativ.';
+$lang['reports_app_name'] = 'Baza Raportet';
+$lang['reports_date_range'] = 'periudhën kohore';
+$lang['reports_device_type_summary'] = 'Device Lloji Përmbledhje';
+$lang['reports_device_types'] = 'Llojet pajisje';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'Raporti i plotë';
+$lang['reports_ip_summary'] = 'IP Përmbledhje';
+$lang['reports_last_30_days'] = 'Fundit 30 Ditët';
+$lang['reports_last_7_days'] = 'Të fundit 7 ditë';
+$lang['reports_live'] = 'jetojnë';
+$lang['reports_range_invalid'] = 'Varg është i pavlefshëm.';
+$lang['reports_report_data'] = 'Raporti i të dhënave';
+$lang['reports_report_settings'] = 'Raporti Settings';
+$lang['reports_reports'] = 'raportet';
+$lang['reports_today'] = 'sot';
+$lang['reports_top_device_types'] = 'Top Llojet Device';
+$lang['reports_top_external_ips'] = 'Top IP e jashtme';
+$lang['reports_top_ips'] = 'IP Top';
+$lang['reports_top_users'] = 'Përdorues kryesues';
+$lang['reports_user_summary'] = 'User Përmbledhje';
+$lang['reports_yesterday'] = 'Dje';
