3216e91735b77f9aa210eb9bb97798ff8e378edd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:43:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'The Tushe Rahotanni app bayar da wani sa na misali rahoton kayayyakin aiki, don aiki da tsarin.';
+$lang['reports_app_name'] = 'tushe Rahotanni';
+$lang['reports_date_range'] = 'kwanan Range';
+$lang['reports_device_type_summary'] = 'Na&#39;ura Type Summary';
+$lang['reports_device_types'] = 'Na&#39;ura Iri';
+$lang['reports_entries'] = 'Entries';
+$lang['reports_full_report'] = 'full Report';
+$lang['reports_ip_summary'] = 'IP Summary';
+$lang['reports_last_30_days'] = 'Last 30 Days';
+$lang['reports_last_7_days'] = 'Last 7 Hari';
+$lang['reports_live'] = 'Live';
+$lang['reports_range_invalid'] = 'Range ba daidai ba ne.';
+$lang['reports_report_data'] = 'Report Data';
+$lang['reports_report_settings'] = 'Report Saituna';
+$lang['reports_reports'] = 'rahotanni';
+$lang['reports_today'] = 'yau';
+$lang['reports_top_device_types'] = 'Top Na&#39;ura Iri';
+$lang['reports_top_external_ips'] = 'Top External IPS';
+$lang['reports_top_ips'] = 'top IPS';
+$lang['reports_top_users'] = 'top Users';
+$lang['reports_user_summary'] = 'User Summary';
+$lang['reports_yesterday'] = 'jiya';
