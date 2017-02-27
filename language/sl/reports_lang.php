901eb33e6c16fea80c8a5ef5dd1fd66718ae7245
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:49:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'Aplikacija Osnovni Poročila ponuja nabor standardnih poročil orodij za operacijski sistem.';
+$lang['reports_app_name'] = 'Navadne Poročila';
+$lang['reports_date_range'] = 'Časovno obdobje';
+$lang['reports_device_type_summary'] = 'Vrsta naprave Povzetek';
+$lang['reports_device_types'] = 'Vrste naprav';
+$lang['reports_entries'] = 'vpisi';
+$lang['reports_full_report'] = 'celotno poročilo';
+$lang['reports_ip_summary'] = 'IP Povzetek';
+$lang['reports_last_30_days'] = 'Zadnjih 30 dni';
+$lang['reports_last_7_days'] = 'Zadnjih 7 dni';
+$lang['reports_live'] = 'živo';
+$lang['reports_range_invalid'] = 'Območje je neveljavna.';
+$lang['reports_report_data'] = 'poročilo podatkov';
+$lang['reports_report_settings'] = 'Nastavitve poročilo';
+$lang['reports_reports'] = 'Poročila';
+$lang['reports_today'] = 'danes';
+$lang['reports_top_device_types'] = 'Top Vrste naprav';
+$lang['reports_top_external_ips'] = 'Top Zunanja IP';
+$lang['reports_top_ips'] = 'Top IP';
+$lang['reports_top_users'] = 'Top uporabniki';
+$lang['reports_user_summary'] = 'uporabnik Povzetek';
+$lang['reports_yesterday'] = 'včeraj';
