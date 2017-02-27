428733d1fc7bdabcb956f882f7eebf305c2a0d1f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 02:50:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/reports_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['reports_app_description'] = 'பேஸ் அறிக்கைகள் பயன்பாட்டை இயக்க அமைப்பு நிலையான அறிக்கை கருவிகளின் தொகுப்பை வழங்குகிறது.';
+$lang['reports_app_name'] = 'பேஸ் அறிக்கைகள்';
+$lang['reports_date_range'] = 'தேதி வரம்பு';
+$lang['reports_device_type_summary'] = 'சாதன வகை சுருக்கம்';
+$lang['reports_device_types'] = 'சாதன வகையான';
+$lang['reports_entries'] = 'பதிவுகள்';
+$lang['reports_full_report'] = 'முழு அறிக்கை';
+$lang['reports_ip_summary'] = 'ஐபி சுருக்கம்';
+$lang['reports_last_30_days'] = 'கடந்த 30 நாட்களில்';
+$lang['reports_last_7_days'] = 'கடந்த 7 நாட்கள்';
+$lang['reports_live'] = 'நேரடி';
+$lang['reports_range_invalid'] = 'வரம்பு செல்லாதது.';
+$lang['reports_report_data'] = 'அறிக்கை தரவு';
+$lang['reports_report_settings'] = 'அறிக்கை அமைப்புகள்';
+$lang['reports_reports'] = 'அறிக்கைகள்';
+$lang['reports_today'] = 'இன்று';
+$lang['reports_top_device_types'] = 'சிறந்த சாதன வகையான';
+$lang['reports_top_external_ips'] = 'சிறந்த வெளி ஐபிஎஸ்';
+$lang['reports_top_ips'] = 'சிறந்த ஐபிஎஸ்';
+$lang['reports_top_users'] = 'சிறந்த பயனர்கள்';
+$lang['reports_user_summary'] = 'பயனர் சுருக்கம்';
+$lang['reports_yesterday'] = 'நேற்று';
